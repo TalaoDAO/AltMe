52702e57256b6976446469d00fb93f49e1627852
hawkbee1 <googandads@gmail.com>
2022-01-13 16:30:40 +0100

    very good ventures initialization

---
+++ android/app/src/main/kotlin/co/talao/crypto/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.talao.crypto.ssi_crypto_wallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:ssi_crypto_wallet/app/app.dart';
+import 'package:ssi_crypto_wallet/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
