0eced3ef0c6a9b991798632d714d3c494b432fe6
taleb <taleb.r75@gmail.com>
2022-03-31 09:33:23 +0430

    writing test for secure storage

--- packages/secure_storage/test/src/secure_storage_test.dart
+++ packages/secure_storage/test/src/secure_storage_test.dart
@@ -3,9 +3,46 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:secure_storage/secure_storage.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late SecureStorageProvider secureStorageProvider;
+
+  setUp(() async {
+    secureStorageProvider = SecureStorageProvider.instance;
+    await secureStorageProvider.deleteAll();
+  });
+
   group('SecureStorage', () {
+
     test('can be instantiated', () {
-      expect(SecureStorageProvider.instance, isNotNull);
+      expect(secureStorageProvider, isNotNull);
+    });
+
+    test('set method', () async {
+      await secureStorageProvider.set('key1', 'value');
+      final result = await secureStorageProvider.get('key1');
+      expect(result, equals('value'));
+    });
+
+    test('delete method', () async {
+      await secureStorageProvider.set('key','value');
+      await secureStorageProvider.delete('key');
+      final result = await secureStorageProvider.get('key');
+      expect(result, isNull);
+    });
+
+    test('deleteAll method', () async {
+      await secureStorageProvider.set('key1', 'value1');
+      await secureStorageProvider.set('key2', 'value2');
+      await secureStorageProvider.deleteAll();
+      final result = await secureStorageProvider.getAllValues();
+      expect(result, '{}');
+    });
+
+    test('containsKey', () async {
+      const key = 'testKey';
+      expect(await secureStorageProvider.get(key), isNull);
+      await secureStorageProvider.set(key, 'test');
+      expect(await secureStorageProvider.get(key), isNotNull);
     });
   });
 }
