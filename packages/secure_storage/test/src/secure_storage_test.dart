1cab89d64b95af6fcda979fe62680f5f088caee2
taleb <taleb.r75@gmail.com>
2022-04-01 12:07:15 +0430

    use mocktail instead of mockito

--- packages/secure_storage/test/src/secure_storage_test.dart
+++ packages/secure_storage/test/src/secure_storage_test.dart
@@ -1,62 +1,77 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:secure_storage/secure_storage.dart';
+import 'package:test/test.dart';
 
 class MockSecureStorageProvider extends Mock implements SecureStorageProvider {}
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  late SecureStorageProvider secureStorageProvider;
+  const key = 'key';
+  const value = 'value';
+  const key1 = 'key1';
+  const value1 = 'value1';
 
-  setUp(() {
-    secureStorageProvider = MockSecureStorageProvider();
+  final SecureStorageProvider secureStorageProvider =
+      MockSecureStorageProvider();
+
+  setUp(() async {
+    when(secureStorageProvider.deleteAll).thenAnswer((invocation) async {});
+    await secureStorageProvider.deleteAll();
   });
-
 
   group('SecureStorage', () {
     test('can be instantiated', () {
       expect(secureStorageProvider, isNotNull);
     });
 
-    //call this
     test('set method', () async {
       when(
-            () => secureStorageProvider.set('key1', 'value'),
-      ).thenAnswer((_) => Future.value);
+        () => secureStorageProvider.set(key, value),
+      ).thenAnswer((invocation) async {});
 
-      await secureStorageProvider.set('key1', 'value');
+      await secureStorageProvider.set(key, value);
 
       when(
-            () => secureStorageProvider.get('key1'),
-      ).thenAnswer((_) {
-        return () => Future<String>.value('value');
-      });
+        () => secureStorageProvider.get(key),
+      ).thenAnswer((invocation) => Future.value(value));
 
-      final result = await secureStorageProvider.get('key1');
-      expect(result, equals('value'));
+      final result = await secureStorageProvider.get(key);
+      expect(result, equals(value));
     });
 
     test('delete method', () async {
-      await secureStorageProvider.set('key', 'value');
-      await secureStorageProvider.delete('key');
-      final result = await secureStorageProvider.get('key');
+      when(() => secureStorageProvider.set(key, value))
+          .thenAnswer((invocation) async {});
+
+      when(() => secureStorageProvider.delete(key))
+          .thenAnswer((invocation) async {});
+
+      when(() => secureStorageProvider.get(key))
+          .thenAnswer((invocation) => Future.value(null));
+
+      await secureStorageProvider.set(key, value);
+      await secureStorageProvider.delete(key);
+      final result = await secureStorageProvider.get(key);
       expect(result, isNull);
     });
 
     test('deleteAll method', () async {
-      await secureStorageProvider.set('key1', 'value1');
-      await secureStorageProvider.set('key2', 'value2');
+      when(() => secureStorageProvider.set(key, value))
+          .thenAnswer((invocation) async {});
+      when(() => secureStorageProvider.set(key1, value1))
+          .thenAnswer((invocation) async {});
+
+      await secureStorageProvider.set(key, value);
+      await secureStorageProvider.set(key1, value1);
+
+      when(secureStorageProvider.deleteAll).thenAnswer((invocation) async {});
+      when(secureStorageProvider.getAllValues)
+          .thenAnswer((invocation) => Future.value(<String, dynamic>{}));
+
       await secureStorageProvider.deleteAll();
       final result = await secureStorageProvider.getAllValues();
-      expect(result, '{}');
+      expect(result, equals(<String,dynamic>{}));
     });
 
-    test('containsKey', () async {
-      const key = 'testKey';
-      expect(await secureStorageProvider.get(key), isNull);
-      await secureStorageProvider.set(key, 'test');
-      expect(await secureStorageProvider.get(key), isNotNull);
-    });
   });
 }
