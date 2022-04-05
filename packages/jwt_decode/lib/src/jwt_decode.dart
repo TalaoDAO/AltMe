97e9ced5cfe34816f6683e283cb93750474460c3
taleb <taleb.r75@gmail.com>
2022-04-05 08:21:51 +0430

    port jwt-package code from talao wallet

--- packages/jwt_decode/lib/src/jwt_decode.dart
+++ packages/jwt_decode/lib/src/jwt_decode.dart
@@ -1,7 +1,40 @@
+import 'dart:convert';
+
 /// {@template jwt_decode}
 /// JWT decode package
 /// {@endtemplate}
-class JwtDecode {
-  /// {@macro jwt_decode}
-  const JwtDecode();
+class JWTDecode {
+  ///parseJwt
+  Map<String, dynamic> parseJwt(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      throw Exception('Invalid Token');
+    }
+
+    final payload = _decodeBase64(parts[1]);
+
+    final dynamic payloadMap = json.decode(payload);
+    if (payloadMap is! Map<String, dynamic>) {
+      throw Exception('Invalid Payload');
+    }
+    return payloadMap;
+  }
+
+  String _decodeBase64(String str) {
+    var output = str.replaceAll('-', '+').replaceAll('_', '/');
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw Exception('Illegal base64url string!"');
+    }
+
+    return utf8.decode(base64Url.decode(output));
+  }
 }
