1fd1a76f0006e8c92e30aec6f824fcd900494e3f
taleb <taleb.r75@gmail.com>
2022-04-06 10:51:19 +0430

    write anoter test for valid token with exact result

--- packages/jwt_decode/test/src/jwt_decode_test.dart
+++ packages/jwt_decode/test/src/jwt_decode_test.dart
@@ -31,6 +31,39 @@ void main() {
       'wDBTOHX1DEC83Os26IT-6VGZpWPjCpOOk5p4EzO1zz09zgR962cNEnlgx62GhJi5ATfnCk'
       'VvqgKjEw171KNfotzlAp84Gqw0m5Ef0KzmLKSzD9pssFZA';
 
+  const validJwtTokenOne =
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImRpZDp3ZWI6dGFsYW8uY28ja2V5'
+      'LTIifQ.eyJzY29wZSI6Im9wZW5pZCIsInJlc3BvbnNlX3R5cGUiOiJpZF90b2tlbiIsImNsa'
+      'WVudF9pZCI6ImRpZDp3ZWI6dGFsYW8uY28iLCJyZWRpcmVjdF91cmkiOiJodHRwczovL3Rhb'
+      'GFvLmNvL2dhaWF4L2xvZ2luX3JlZGlyZWN0LzI4N2Y1OGU5LWE1MGMtMTFlYy1iZWEwLTBhM'
+      'TYyODk1ODU2MCIsInJlc3BvbnNlX21vZGUiOiJwb3N0IiwiY2xhaW1zIjoie1wiaWRfdG9rZ'
+      'W5cIjp7fSxcInZwX3Rva2VuXCI6e1wicHJlc2VudGF0aW9uX2RlZmluaXRpb25cIjp7XCJpZ'
+      'FwiOlwicGFzc19mb3JfZ2FpYXhcIixcImlucHV0X2Rlc2NyaXB0b3JzXCI6W3tcImlkXCI6X'
+      'CJHYWlheFBhc3MgaXNzdWVkIGJ5IFRhbGFvXCIsXCJwdXJwb3NlXCI6XCJUZXN0IGZvciBHY'
+      'WlhLVggaGFja2F0aG9uXCIsXCJmb3JtYXRcIjp7XCJsZHBfdmNcIjp7XCJwcm9vZl90eXBlX'
+      'CI6W1wiSnNvbldlYlNpZ25hdHVyZTIwMjBcIl19fSxcImNvbnN0cmFpbnRzXCI6e1wibGlta'
+      'XRfZGlzY2xvc3VyZVwiOlwicmVxdWlyZWRcIixcImZpZWxkc1wiOlt7XCJwYXRoXCI6W1wiJ'
+      'C5jcmVkZW50aWFsU3ViamVjdC50eXBlXCJdLFwicHVycG9zZVwiOlwiT25lIGNhbiBvbmx5I'
+      'GFjY2VwdCBHYWlheFBhc3NcIixcImZpbHRlclwiOntcInR5cGVcIjpcInN0cmluZ1wiLFwic'
+      'GF0dGVyblwiOlwiR2FpYXhQYXNzXCJ9fSx7XCJwYXRoXCI6W1wiJC5pc3N1ZXJcIl0sXCJwd'
+      'XJwb3NlXCI6XCJPbmUgY2FuIGFjY2VwdCBvbmx5IEdhaWF4UGFzcyBzaWduZWQgYnkgVGFsY'
+      'W9cIixcImZpbHRlclwiOntcInR5cGVcIjpcInN0cmluZ1wiLFwicGF0dGVyblwiOlwiZGlkO'
+      'ndlYjp0YWxhby5jb1wifX1dfX1dfX19Iiwibm9uY2UiOiI2ajBSQVRaZUlqIiwicmVnaXN0c'
+      'mF0aW9uIjoie1wiaWRfdG9rZW5fc2lnbmluZ19hbGdfdmFsdWVzX3N1cHBvcnRlZFwiOltcI'
+      'lJTMjU2XCIsXCJFUzI1NlwiLFwiRVMyNTZLXCIsXCJFZERTQVwiXSxcInN1YmplY3Rfc3lud'
+      'GF4X3R5cGVzX3N1cHBvcnRlZFwiOltcImRpZDp3ZWJcIixcImRpZDp0elwiLFwiZGlkOmtle'
+      'VwiLFwiZGlkOmlvblwiLFwiZGlkOnBraFwiLFwiZGlkOmV0aHJcIl19IiwicmVxdWVzdF91c'
+      'mkiOiJodHRwczovL3RhbGFvLmNvL2dhaWF4L2xvZ2luX3JlcXVlc3RfdXJpLzI4N2Y1OGU5L'
+      'WE1MGMtMTFlYy1iZWEwLTBhMTYyODk1ODU2MCJ9.XU17UUTA1CZU6vHtV5nOxVYb9J6lI2jj'
+      '9GDn8aoRiOhFjvEbylg_ycCHBSZmR6aoAXTVv9bQxBvvg6z0UmxEeo29slVLzSFVvmysY5nL'
+      '3zw5I0A-IBnpHxMkt44GNIZTJEnrkne7bdOSYhxETt4cE42ZXAA61MJIu--iFJ53E_CnhKym'
+      'wn7yCoddt9RRVIiOYKEegBMTXsOO5HnQDcxv39vpuHxmAuOd1seZB8zZs7cTtqcKRjU_YnuB'
+      'CZwF0xGHCe6Su5zgfXKivvaTYrXK0Bgl3y614vN3_qSXFeJ-CoLyy0AJkIYxvxD7PKMHswRr'
+      'Y-NVqJ6_YmUo3uDIr9BmSw';
+
+  const jsonStringOfValidTokenOne =
+      '''{scope: openid, response_type: id_token, client_id: did:web:talao.co, redirect_uri: https://talao.co/gaiax/login_redirect/287f58e9-a50c-11ec-bea0-0a1628958560, response_mode: post, claims: {"id_token":{},"vp_token":{"presentation_definition":{"id":"pass_for_gaiax","input_descriptors":[{"id":"GaiaxPass issued by Talao","purpose":"Test for Gaia-X hackathon","format":{"ldp_vc":{"proof_type":["JsonWebSignature2020"]}},"constraints":{"limit_disclosure":"required","fields":[{"path":["\$.credentialSubject.type"],"purpose":"One can only accept GaiaxPass","filter":{"type":"string","pattern":"GaiaxPass"}},{"path":["\$.issuer"],"purpose":"One can accept only GaiaxPass signed by Talao","filter":{"type":"string","pattern":"did:web:talao.co"}}]}}]}}}, nonce: 6j0RATZeIj, registration: {"id_token_signing_alg_values_supported":["RS256","ES256","ES256K","EdDSA"],"subject_syntax_types_supported":["did:web","did:tz","did:key","did:ion","did:pkh","did:ethr"]}, request_uri: https://talao.co/gaiax/login_request_uri/287f58e9-a50c-11ec-bea0-0a1628958560}''';
+
   const inValidJwtTokenWithLessThanThreePart = 'partOne.partTwo';
   const inValidJwtTokenWithThreePart = 'partOne.partTwo.partThree';
 
@@ -46,7 +79,13 @@ void main() {
     test('valid jwt token', () {
       final result = jwtDecode.parseJwt(validJwtToken);
       expect(result, isNotNull);
-      expect(result, isA<Map<String,dynamic>>());
+      expect(result, isA<Map<String, dynamic>>());
+    });
+
+    test('another valid jwt token with result', () {
+      final result = jwtDecode.parseJwt(validJwtTokenOne);
+      expect(result, isNotNull);
+      expect(result.toString(), equals(jsonStringOfValidTokenOne));
     });
 
     test('inValid jwt token with less than three part', () {
