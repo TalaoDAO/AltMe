c474f84f51155e09388159c6f6e3aaa7d23f348b
taleb <taleb.r75@gmail.com>
2022-04-12 12:29:20 +0430

    write mock

--- packages/did_kit/test/src/did_kit_test.dart
+++ packages/did_kit/test/src/did_kit_test.dart
@@ -1,11 +1,18 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:did_kit/src/didkit_io.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:did_kit/did_kit.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDidKit extends Mock implements DIDKitIO {}
+
 
 void main() {
+  final mockDidKit = MockDidKit();
+
   group('DidKit', () {
-    test('can be instantiated', () {
-      expect(DidKit(), isNotNull);
+    test('verify did kit version is 0.3', () {
+      when(mockDidKit.getVersion).thenReturn('0.3');
+      expect(mockDidKit.getVersion(), '0.3');
     });
   });
 }
