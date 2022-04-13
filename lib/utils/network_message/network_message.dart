e78efcea1dd35e6e79d75b7b85ed67e69c378767
Bibash Shrestha <bibashshrestha@gmail.com>
2022-04-13 16:25:43 +0545

    localisation text added

--- lib/utils/network_message/network_message.dart
+++ lib/utils/network_message/network_message.dart
@@ -1,65 +1,14 @@
+import 'package:altme/l10n/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:network/network.dart';
 
 class NetworkMessage {
-  ///getErrorMessage
-  String getErrorMessage(
-      BuildContext context, NetworkException networkException) {
-    final localizations = AppLocalizations.of(context);
-
-    var errorMessage = '';
-    networkException.when(notImplemented: () {
-      errorMessage = localizations.networkErrorNotImplemented;
-    }, requestCancelled: () {
-      errorMessage = localizations.networkErrorRequestCancelled;
-    }, internalServerError: () {
-      errorMessage = localizations.networkErrorInternalServerError;
-    }, notFound: (String reason) {
-      errorMessage = reason;
-    }, serviceUnavailable: () {
-      errorMessage = localizations.networkErrorServiceUnavailable;
-    }, methodNotAllowed: () {
-      errorMessage = localizations.networkErrorMethodNotAllowed;
-    }, badRequest: () {
-      errorMessage = localizations.networkErrorBadRequest;
-    }, unauthorizedRequest: () {
-      errorMessage = localizations.networkErrorUnauthorizedRequest;
-    }, unexpectedError: () {
-      errorMessage = localizations.networkErrorUnexpectedError;
-    }, requestTimeout: () {
-      errorMessage = localizations.networkErrorRequestTimeout;
-    }, noInternetConnection: () {
-      errorMessage = localizations.networkErrorNoInternetConnection;
-    }, conflict: () {
-      errorMessage = localizations.networkErrorConflict;
-    }, sendTimeout: () {
-      errorMessage = localizations.networkErrorSendTimeout;
-    }, unableToProcess: () {
-      errorMessage = localizations.networkErrorUnableToProcess;
-    }, defaultError: (String error) {
-      errorMessage = error;
-    }, formatException: () {
-      errorMessage = localizations.networkErrorUnexpectedError;
-    }, notAcceptable: () {
-      errorMessage = localizations.networkErrorNotAcceptable;
-    }, created: () {
-      errorMessage = localizations.networkErrorCreated;
-    }, gatewayTimeout: () {
-      errorMessage = localizations.networkErrorGatewayTimeout;
-    }, ok: () {
-      errorMessage = localizations.networkErrorOk;
-    }, tooManyRequests: () {
-      errorMessage = localizations.networkErrorTooManyRequests;
-    }, unauthenticated: () {
-      errorMessage = localizations.networkErrorUnauthenticated;
-    });
-    return errorMessage;
-  }
-
   ///displayError
   void displayError(
-      BuildContext context, NetworkException error, Color errorColor) {
+    BuildContext context,
+    NetworkException error,
+    Color errorColor,
+  ) {
     final errorMessage = getErrorMessage(context, error);
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -67,5 +16,84 @@ class NetworkMessage {
         content: Text(errorMessage),
       ),
     );
+  }
+
+  ///getErrorMessage
+  String getErrorMessage(
+    BuildContext context,
+    NetworkException networkException,
+  ) {
+    final l10n = context.l10n;
+
+    var errorMessage = '';
+    networkException.when(
+      notImplemented: () {
+        errorMessage = l10n.networkErrorNotImplemented;
+      },
+      requestCancelled: () {
+        errorMessage = l10n.networkErrorRequestCancelled;
+      },
+      internalServerError: () {
+        errorMessage = l10n.networkErrorInternalServerError;
+      },
+      notFound: (String reason) {
+        errorMessage = reason;
+      },
+      serviceUnavailable: () {
+        errorMessage = l10n.networkErrorServiceUnavailable;
+      },
+      methodNotAllowed: () {
+        errorMessage = l10n.networkErrorMethodNotAllowed;
+      },
+      badRequest: () {
+        errorMessage = l10n.networkErrorBadRequest;
+      },
+      unauthorizedRequest: () {
+        errorMessage = l10n.networkErrorUnauthorizedRequest;
+      },
+      unexpectedError: () {
+        errorMessage = l10n.networkErrorUnexpectedError;
+      },
+      requestTimeout: () {
+        errorMessage = l10n.networkErrorRequestTimeout;
+      },
+      noInternetConnection: () {
+        errorMessage = l10n.networkErrorNoInternetConnection;
+      },
+      conflict: () {
+        errorMessage = l10n.networkErrorConflict;
+      },
+      sendTimeout: () {
+        errorMessage = l10n.networkErrorSendTimeout;
+      },
+      unableToProcess: () {
+        errorMessage = l10n.networkErrorUnableToProcess;
+      },
+      defaultError: (String error) {
+        errorMessage = error;
+      },
+      formatException: () {
+        errorMessage = l10n.networkErrorUnexpectedError;
+      },
+      notAcceptable: () {
+        errorMessage = l10n.networkErrorNotAcceptable;
+      },
+      created: () {
+        errorMessage = l10n.networkErrorCreated;
+      },
+      gatewayTimeout: () {
+        errorMessage = l10n.networkErrorGatewayTimeout;
+      },
+      ok: () {
+        errorMessage = l10n.networkErrorOk;
+      },
+      tooManyRequests: () {
+        errorMessage = l10n.networkErrorTooManyRequests;
+      },
+      unauthenticated: () {
+        errorMessage = l10n.networkErrorUnauthenticated;
+      },
+    );
+    return errorMessage;
   }
 }
