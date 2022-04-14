3da60b915ddba7833068ac9a1d910363ffb24eb2
taleb <taleb.r75@gmail.com>
2022-04-14 13:24:06 +0430

    import code of ImageFromNetwork widget

---
+++ lib/app/shared/widget/image_from_network/image_from_network.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ImageFromNetwork extends StatelessWidget {
+  const ImageFromNetwork(
+    this.url, {
+    Key? key,
+    this.fit,
+  }) : super(key: key);
+
+  final String url;
+  final BoxFit? fit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      url,
+      fit: fit,
+      loadingBuilder: (context, child, loadingProgress) =>
+          (loadingProgress == null) ? child : const CircularProgressIndicator(),
+      errorBuilder: (context, error, stackTrace) => const SizedBox.shrink(),
+    );
+  }
+}
