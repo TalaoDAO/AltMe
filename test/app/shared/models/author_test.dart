09fdb1bb84d0ed4e93568ecba8a0d34d3e8cb2cd
taleb <taleb.r75@gmail.com>
2022-04-15 11:20:23 +0430

    wrote test for Author model

---
+++ test/app/shared/models/author_test.dart
@@ -0,0 +1,32 @@
+import 'package:altme/app/shared/models/author/author.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Author model work properly', () {
+
+    test('initialize Author with default constructor', () {
+      final author = Author('Taleb', 'logo');
+      expect(author.name, 'Taleb');
+      expect(author.logo, 'logo');
+    });
+
+    test('initialize Author fromJson constructor', () {
+      const authorJson = <String, dynamic>{
+        'name': 'Taleb',
+        'logo': 'logo',
+      };
+      final author = Author.fromJson(authorJson);
+      expect(author.name, 'Taleb');
+      expect(author.logo, 'logo');
+    });
+
+    test('Author toJson worked correct', () {
+      const authorJson = <String, dynamic>{
+        'name': 'Taleb',
+        'logo': 'logo',
+      };
+      final author = Author.fromJson(authorJson);
+      expect(author.toJson(), authorJson);
+    });
+  });
+}
