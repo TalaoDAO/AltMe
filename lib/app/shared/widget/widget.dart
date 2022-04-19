10b32fa0b52305d54053d286de82ac63b803f48d
taleb <taleb.r75@gmail.com>
2022-04-19 15:11:10 +0430

    eport ImageFromNetwork into widget.dart file

--- lib/app/shared/widget/widget.dart
+++ lib/app/shared/widget/widget.dart
@@ -2,4 +2,5 @@ export 'back_leading_button.dart';
 export 'base/base.dart';
 export 'custom_app_bar.dart';
 export 'hero_workaround.dart';
+export 'image_from_network.dart';
 export 'tooltip_text.dart';
