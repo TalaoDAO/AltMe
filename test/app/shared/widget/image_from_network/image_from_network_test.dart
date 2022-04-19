564220726b56cff1e66a87cdd641f8a3341fbc02
taleb <taleb.r75@gmail.com>
2022-04-19 14:12:50 +0430

    cover 100% test for ImageFromNetwork widget

--- test/app/shared/widget/image_from_network/image_from_network_test.dart
+++ test/app/shared/widget/image_from_network/image_from_network_test.dart
@@ -1,108 +1,55 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:altme/app/shared/widget/image_from_network/image_from_network.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+
 
 void main() {
-  testWidgets('Get image widget with good url', (tester) async {
-    await mockNetworkImages(() async {
-      await tester.pumpWidget(const ImageFromNetwork('success'));
-      expect(find.byType(Image), findsOneWidget);
-      expect(find.byType(SizedBox), findsNothing);
-    });
-  });
-  testWidgets('get SizedBox on error', (tester) async {
-    final key = GlobalKey();
-    await mockNetworkImages(() async {
-      await tester.pumpWidget(
-        ImageFromNetwork('https://toto.fr', key: key),
-      );
-      //expect(find.byType(SizedBox), findsOneWidget);
-    });
-  });
-}
 
-/// Based on  mocktail_image_network (https://pub.dev/packages/mocktail_image_network)
+  const networkImageUrl = 'https://www.demo.com';
+  final widgetKey = GlobalKey();
 
-T mockNetworkImages<T>(T Function() body) {
-  return HttpOverrides.runZoned(
-    body,
-    createHttpClient: (_) => _createHttpClient(),
+  Widget makeTestableWidget() => MaterialApp(
+    home: ImageFromNetwork(networkImageUrl,key: widgetKey,),
   );
-}
 
-class _MockHttpClient extends Mock implements HttpClient {
-  _MockHttpClient() {
-    registerFallbackValue((List<int> _) {});
-    registerFallbackValue(Uri());
-  }
-
-  @override
-  set autoUncompress(bool _autoUncompress) {}
-
-  @override
-  bool get autoUncompress => false;
-}
-
-class _MockHttpClientRequest extends Mock implements HttpClientRequest {}
-
-class _MockHttpClientResponse extends Mock implements HttpClientResponse {}
-
-class _MockHttpHeaders extends Mock implements HttpHeaders {}
-
-HttpClient _createHttpClient() {
-  final client = _MockHttpClient();
-  final request = _MockHttpClientRequest();
-  final response = _MockHttpClientResponse();
-  final headers = _MockHttpHeaders();
-  when(() => response.compressionState)
-      .thenReturn(HttpClientResponseCompressionState.notCompressed);
-  when(() => response.contentLength).thenReturn(_transparentPixelPng.length);
-  when(() => response.statusCode).thenReturn(HttpStatus.ok);
-  when(
-    () => response.listen(
-      any(),
-      onDone: any(named: 'onDone'),
-      onError: any(named: 'onError'),
-      cancelOnError: any(named: 'cancelOnError'),
-    ),
-  ).thenAnswer((invocation) {
-    final onData =
-        invocation.positionalArguments[0] as void Function(List<int>);
-    final onDone = invocation.namedArguments[#onDone] as void Function()?;
-    return Stream<List<int>>.fromIterable(<List<int>>[_transparentPixelPng])
-        .listen(onData, onDone: onDone);
-  });
-  when(() => request.headers).thenReturn(headers);
-  when(() => client.getUrl(any())).thenAnswer((_) async {
-    when(request.close).thenAnswer((_) async => response);
-    return request;
-  });
-  when(() => client.getUrl(Uri.parse('https://toto.fr'))).thenAnswer((_) async {
-    when(() => response.statusCode).thenReturn(HttpStatus.badRequest);
-    when(
-      () => response.listen(
-        any(),
-        onDone: any(named: 'onDone'),
-        onError: any(named: 'onError'),
-        cancelOnError: any(named: 'cancelOnError'),
-      ),
-    ).thenAnswer((invocation) {
-      throw Exception('not found image at https://toto.fr');
-      // final onData =
-      //     invocation.positionalArguments[0] as void Function(List<int>);
-      // final onDone = invocation.namedArguments[#onDone] as void Function()?;
-      // return Stream<List<int>>.fromIterable(<List<int>>[_transparentPixelPng])
-      //     .listen(onData, onDone: onDone);
+  testWidgets('find Image Widget by key', (tester) async {
+    await mockNetworkImagesFor(() async {
+      await tester.pumpWidget(makeTestableWidget());
+      expect(find.byKey(widgetKey), findsOneWidget);
     });
-    return request;
   });
-  return client;
-}
 
-final _transparentPixelPng = base64Decode(
-  '''iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==''',
-);
+  testWidgets('find Image widget by Type', (tester) async {
+    await mockNetworkImagesFor(() async {
+      await tester.pumpWidget(makeTestableWidget());
+      expect(find.byType(Image), findsOneWidget);
+    });
+  });
+
+  testWidgets('verify all property is correct', (tester) async {
+    await mockNetworkImagesFor(() async {
+      await tester.pumpWidget(makeTestableWidget());
+      final imageFromNetwork =
+          tester.widget<ImageFromNetwork>(find.byType(ImageFromNetwork));
+      expect(imageFromNetwork.url, networkImageUrl);
+      expect(imageFromNetwork.fit, null);
+      expect(imageFromNetwork.key, widgetKey);
+    });
+  });
+
+  testWidgets('get SizedBox on errorBuilder', (tester) async {
+    await mockNetworkImagesFor(() async {
+      await tester.pumpWidget(makeTestableWidget());
+      final image = tester.widget<Image>(find.byType(Image));
+      final BuildContext context = tester.element(find.byType(MaterialApp));
+      final result = image.errorBuilder?.call(
+        context,
+        Object(),
+        StackTrace.fromString('stackTraceString'),
+      );
+      expect(result, isA<SizedBox>());
+    });
+  });
+}
