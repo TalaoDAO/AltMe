26e50a12a7b1bcb688adc5c702df0266037ebe86
taleb <taleb.r75@gmail.com>
2022-04-25 13:52:42 +0430

    import BaseIllustrationPage codes

---
+++ lib/app/shared/widget/base/illustration_page.dart
@@ -0,0 +1,63 @@
+import 'package:altme/app/shared/widget/base/button.dart';
+import 'package:altme/app/shared/widget/base/page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BaseIllustrationPage extends StatelessWidget {
+  const BaseIllustrationPage({
+    Key? key,
+    required this.asset,
+    this.description,
+    required this.action,
+    this.onPressed,
+    this.backgroundColor = Colors.transparent,
+  }) : super(key: key);
+
+  final String asset;
+  final String? description;
+  final String action;
+
+  final VoidCallback? onPressed;
+
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 1.2,
+          color: backgroundColor,
+          child: SvgPicture.asset(asset),
+        ),
+        BasePage(
+          scrollView: false,
+          backgroundColor: Colors.transparent,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              if (description != null && description!.isNotEmpty)
+                Text(
+                  description!,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              Expanded(child: Container()),
+              BaseButton.white(
+                onPressed: onPressed,
+                context: context,
+                child: Text(action),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
