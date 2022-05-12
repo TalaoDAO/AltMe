06fa6b1fac035fe43979381a6b110c5b25dd6d82
hawkbee1 <googandads@gmail.com>
2022-05-12 09:19:09 +0200

    update PassUrl

--- lib/app/shared/constants/urls.dart
+++ lib/app/shared/constants/urls.dart
@@ -1,7 +1,7 @@
 class Urls {
   static const String appContactWebsiteUrl = 'https://altme.io';
-  static const String emailPassUrl = 'https://talao.co/emailpass';
-  static const String phonePassUrl = 'https://talao.co/phonepass';
+  static const String emailPassUrl = 'https://issuer.talao.co/emailpass';
+  static const String phonePassUrl = 'https://issuer.talao.co/phonepass';
   static const String checkIssuerServerUrl =
       'https://talao.co/trusted-issuers-registry/v1/issuers';
 }
