bf0440ce8b85f04a668bed721079cbf8a8736534
taleb <taleb.r75@gmail.com>
2022-05-26 16:12:25 +0430

    change ratio of nft item widget

--- lib/app/shared/constants/sizes.dart
+++ lib/app/shared/constants/sizes.dart
@@ -1,7 +1,5 @@
 abstract class Sizes {
-  static const double nftItemWidth = 150;
-  static const double nftItemHeight = 190;
-  static const double nftItemRatio = nftItemWidth / nftItemHeight;
+  static const double nftItemRatio = 0.78;
 
   static const double tokenLogoSize = 34;
 
