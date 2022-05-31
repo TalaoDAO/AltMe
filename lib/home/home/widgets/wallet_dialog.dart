361507ac35dc165ab247267c286e5a5768020e08
Bibash Shrestha <bibashshrestha@gmail.com>
2022-05-31 15:22:01 +0545

    pop up created for wallet creation

---
+++ lib/home/home/widgets/wallet_dialog.dart
@@ -0,0 +1,56 @@
+import 'package:altme/app/app.dart';
+import 'package:altme/l10n/l10n.dart';
+import 'package:altme/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class WalletDialog extends StatelessWidget {
+  const WalletDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return AlertDialog(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      contentPadding: const EdgeInsets.only(
+        top: 24,
+        bottom: 16,
+        left: 24,
+        right: 24,
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const ImageIcon(
+            AssetImage(IconStrings.emptyWalletAdd),
+            size: 80,
+          ),
+          const SizedBox(height: 15),
+          Text(
+            l10n.create_wallet_message,
+            style: Theme.of(context).textTheme.dialogTitle,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 15),
+          MyElevatedButton(
+            text: l10n.create_wallet.toUpperCase(),
+            verticalSpacing: 8,
+            fontSize: 13,
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
+          MyElevatedButton(
+            text: l10n.restore_wallet.toUpperCase(),
+            verticalSpacing: 8,
+            fontSize: 13,
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/home/widgets/widgets.dart
+++ lib/home/home/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'get_cards_widget.dart';
 export 'qr_icon.dart';
+export 'wallet_dialog.dart';
