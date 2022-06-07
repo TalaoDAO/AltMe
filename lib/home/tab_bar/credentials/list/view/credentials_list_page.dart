1b4036331c879f884728f9cd7e7ef40cb505718c
Bibash Shrestha <bibashshrestha@gmail.com>
2022-06-07 20:16:35 +0545

    decreasing loop

--- lib/home/tab_bar/credentials/list/view/credentials_list_page.dart
+++ lib/home/tab_bar/credentials/list/view/credentials_list_page.dart
@@ -13,215 +13,202 @@ class CredentialsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BasePage(
-      scrollView: true,
-      padding: EdgeInsets.zero,
-      backgroundColor: Theme.of(context).colorScheme.transparent,
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: const [
-          GamingCredentials(),
-          CommunityCredentials(),
-          IdentityCredentials(),
-          OtherCredentials(),
-        ],
-      ),
+    return BlocBuilder<WalletCubit, WalletState>(
+      builder: (context, state) {
+        final gamingCredentials = <CredentialModel>[];
+        final communityCredentials = <CredentialModel>[];
+        final identityCredentials = <CredentialModel>[];
+        final othersCredentials = <CredentialModel>[];
+
+        for (final credential in state.credentials) {
+          switch (credential
+              .credentialPreview.credentialSubjectModel.credentialCategory) {
+            case CredentialCategory.gamingCards:
+              gamingCredentials.add(credential);
+              break;
+            case CredentialCategory.communityCards:
+              communityCredentials.add(credential);
+              break;
+            case CredentialCategory.identityCards:
+              identityCredentials.add(credential);
+              break;
+            case CredentialCategory.othersCards:
+              othersCredentials.add(credential);
+              break;
+          }
+        }
+
+        return BasePage(
+          scrollView: true,
+          padding: EdgeInsets.zero,
+          backgroundColor: Theme.of(context).colorScheme.transparent,
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              GamingCredentials(credentials: gamingCredentials),
+              CommunityCredentials(credentials: communityCredentials),
+              IdentityCredentials(credentials: identityCredentials),
+              OtherCredentials(credentials: othersCredentials),
+            ],
+          ),
+        );
+      },
     );
   }
 }
 
 class GamingCredentials extends StatelessWidget {
-  const GamingCredentials({Key? key}) : super(key: key);
+  const GamingCredentials({Key? key, required this.credentials})
+      : super(key: key);
+
+  final List<CredentialModel> credentials;
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return BlocBuilder<WalletCubit, WalletState>(
-      builder: (context, state) {
-        var _credentialList = <CredentialModel>[];
-        _credentialList = state.credentials
-            .where(
-              (element) =>
-                  element.credentialPreview.credentialSubjectModel
-                      .credentialCategory ==
-                  CredentialCategory.gamingCards,
-            )
-            .toList();
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${l10n.gamingCards} (${_credentialList.length})',
-              style: Theme.of(context).textTheme.credentialCategoryTitle,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 4,
-                mainAxisSpacing: 4,
-                childAspectRatio: 1.2,
-              ),
-              itemCount: _credentialList.length,
-              itemBuilder: (_, index) => CredentialsListPageItem(
-                credentialModel: _credentialList[index],
-              ),
-            ),
-          ],
-        );
-      },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          '${l10n.gamingCards} (${credentials.length})',
+          style: Theme.of(context).textTheme.credentialCategoryTitle,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1.2,
+          ),
+          itemCount: credentials.length,
+          itemBuilder: (_, index) => CredentialsListPageItem(
+            credentialModel: credentials[index],
+          ),
+        ),
+      ],
     );
   }
 }
 
 class CommunityCredentials extends StatelessWidget {
-  const CommunityCredentials({Key? key}) : super(key: key);
+  const CommunityCredentials({Key? key, required this.credentials})
+      : super(key: key);
+
+  final List<CredentialModel> credentials;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WalletCubit, WalletState>(
-      builder: (context, state) {
-        final l10n = context.l10n;
-        var _credentialList = <CredentialModel>[];
-        _credentialList = state.credentials
-            .where(
-              (element) =>
-                  element.credentialPreview.credentialSubjectModel
-                      .credentialCategory ==
-                  CredentialCategory.communityCards,
-            )
-            .toList();
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${l10n.communityCards} (${_credentialList.length})',
-              style: Theme.of(context).textTheme.credentialCategoryTitle,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 4,
-                mainAxisSpacing: 4,
-                childAspectRatio: 1.2,
-              ),
-              itemCount: _credentialList.length,
-              itemBuilder: (_, index) => CredentialsListPageItem(
-                credentialModel: _credentialList[index],
-              ),
-            ),
-          ],
-        );
-      },
+    final l10n = context.l10n;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          '${l10n.communityCards} (${credentials.length})',
+          style: Theme.of(context).textTheme.credentialCategoryTitle,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1.2,
+          ),
+          itemCount: credentials.length,
+          itemBuilder: (_, index) => CredentialsListPageItem(
+            credentialModel: credentials[index],
+          ),
+        ),
+      ],
     );
   }
 }
 
 class IdentityCredentials extends StatelessWidget {
-  const IdentityCredentials({Key? key}) : super(key: key);
+  const IdentityCredentials({Key? key, required this.credentials})
+      : super(key: key);
+
+  final List<CredentialModel> credentials;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WalletCubit, WalletState>(
-      builder: (context, state) {
-        final l10n = context.l10n;
-        var _credentialList = <CredentialModel>[];
-        _credentialList = state.credentials
-            .where(
-              (element) =>
-                  element.credentialPreview.credentialSubjectModel
-                      .credentialCategory ==
-                  CredentialCategory.identityCards,
-            )
-            .toList();
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${l10n.identityCards} (${_credentialList.length})',
-              style: Theme.of(context).textTheme.credentialCategoryTitle,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 4,
-                mainAxisSpacing: 4,
-                childAspectRatio: 1.2,
-              ),
-              itemCount: _credentialList.length,
-              itemBuilder: (_, index) => CredentialsListPageItem(
-                credentialModel: _credentialList[index],
-              ),
-            ),
-          ],
-        );
-      },
+    final l10n = context.l10n;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          '${l10n.identityCards} (${credentials.length})',
+          style: Theme.of(context).textTheme.credentialCategoryTitle,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1.2,
+          ),
+          itemCount: credentials.length,
+          itemBuilder: (_, index) => CredentialsListPageItem(
+            credentialModel: credentials[index],
+          ),
+        ),
+      ],
     );
   }
 }
 
 class OtherCredentials extends StatelessWidget {
-  const OtherCredentials({Key? key}) : super(key: key);
+  const OtherCredentials({Key? key, required this.credentials})
+      : super(key: key);
+
+  final List<CredentialModel> credentials;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WalletCubit, WalletState>(
-      builder: (context, state) {
-        final l10n = context.l10n;
-        var _credentialList = <CredentialModel>[];
-        _credentialList = state.credentials
-            .where(
-              (element) =>
-                  element.credentialPreview.credentialSubjectModel
-                      .credentialCategory ==
-                  CredentialCategory.othersCards,
-            )
-            .toList();
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${l10n.otherCards} (${_credentialList.length})',
-              style: Theme.of(context).textTheme.credentialCategoryTitle,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 4,
-                mainAxisSpacing: 4,
-                childAspectRatio: 1.2,
-              ),
-              itemCount: _credentialList.length,
-              itemBuilder: (_, index) => CredentialsListPageItem(
-                credentialModel: _credentialList[index],
-              ),
-            ),
-          ],
-        );
-      },
+    final l10n = context.l10n;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          '${l10n.otherCards} (${credentials.length})',
+          style: Theme.of(context).textTheme.credentialCategoryTitle,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1.2,
+          ),
+          itemCount: credentials.length,
+          itemBuilder: (_, index) => CredentialsListPageItem(
+            credentialModel: credentials[index],
+          ),
+        ),
+      ],
     );
   }
 }
