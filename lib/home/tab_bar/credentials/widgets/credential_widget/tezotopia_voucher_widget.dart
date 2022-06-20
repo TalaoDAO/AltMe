8bb82624d6549c33bb9ae6f96fb6ba15e9e74cf5
Bibash Shrestha <bibashshrestha@gmail.com>
2022-06-20 11:19:13 +0545

    remove back image for tezotopia card

--- lib/home/tab_bar/credentials/widgets/credential_widget/tezotopia_voucher_widget.dart
+++ lib/home/tab_bar/credentials/widgets/credential_widget/tezotopia_voucher_widget.dart
@@ -40,37 +40,12 @@ class TezotopiaVoucherDisplayDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        CardAnimation(
-          recto: TezotopiaVoucherRecto(),
-          verso: TezotopiaVoucherVerso(),
-        ),
-      ],
-    );
+    return const TezotopiaVoucherRecto();
   }
 }
 
 class TezotopiaVoucherRecto extends Recto {
   const TezotopiaVoucherRecto({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return CredentialImage(
-      image: ImageStrings.tezotopiaVoucher,
-      child: AspectRatio(
-        /// size from over18 recto picture
-        aspectRatio: 584 / 317,
-        child: CustomMultiChildLayout(
-          delegate: TezotopiaVoucherVersoDelegate(position: Offset.zero),
-        ),
-      ),
-    );
-  }
-}
-
-class TezotopiaVoucherVerso extends Verso {
-  const TezotopiaVoucherVerso({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
