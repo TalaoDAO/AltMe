44d8d1f65ff0090f7eb83896ececc3a1bd81b7b2
hawkbee1 <googandads@gmail.com>
2022-06-21 11:59:53 +0200

    Small meeting modification

--- lib/home/home/view/home_page.dart
+++ lib/home/home/view/home_page.dart
@@ -58,7 +58,6 @@ class _HomePageState extends State<HomePage> {
             scaffoldKey.currentState!.openDrawer();
           },
         ),
-        titleTrailing: const GetCardsWidget(),
         body: Stack(
           children: [
             Column(
