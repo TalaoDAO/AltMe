d65bf0f2ea99b6937b9ed7a665d631cd4b00fc48
hawkbee1 <googandads@gmail.com>
2022-06-22 15:45:36 +0200

    Don't display NFT with balance = 0 (not owned anymore)

--- lib/home/tab_bar/nft/cubit/nft_cubit.dart
+++ lib/home/tab_bar/nft/cubit/nft_cubit.dart
@@ -38,6 +38,9 @@ class NftCubit extends Cubit<NftState> {
           .where(
             (dynamic json) => json['displayUri'] != null,
           )
+          .where(
+            (dynamic json) => json['balance'] != '0',
+          )
           .map((dynamic e) => NftModel.fromJson(e as Map<String, dynamic>))
           .toList();
       emit(state.populate(data: data));
