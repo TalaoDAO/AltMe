fc3d4a712573bb08ae3d7fa3a97326d260c1bfaa
Bibash Shrestha <bibashshrestha@gmail.com>
2022-07-20 19:54:23 +0545

    some changes

--- lib/dashboard/tab_bar/credentials/widgets/default_display_descriptor.dart
+++ lib/dashboard/tab_bar/credentials/widgets/default_display_descriptor.dart
@@ -16,53 +16,57 @@ class DefaultDisplayDescriptor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final credential = Credential.fromJsonOrDummy(credentialModel.data);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          flex: 2,
-          child: FractionallySizedBox(
-            widthFactor: 0.95,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CredentialIcon(
-                      iconData: credential
-                          .credentialSubjectModel.credentialSubjectType
-                          .iconData(),
-                      size: 20,
-                    ),
-                    const SizedBox(width: 2),
-                    Expanded(
-                      child: DisplayNameCard(
-                        credentialModel: credentialModel,
-                        style: Theme.of(context).textTheme.credentialTitle,
+    return FractionallySizedBox(
+      widthFactor: 0.95,
+      heightFactor: 0.9,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: FractionallySizedBox(
+              widthFactor: 0.95,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      CredentialIcon(
+                        iconData: credential
+                            .credentialSubjectModel.credentialSubjectType
+                            .iconData(),
+                        size: 20,
                       ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 5),
-                Expanded(
-                  child: DisplayDescriptionCard(
-                    credentialModel: credentialModel,
-                    style: Theme.of(context).textTheme.credentialDescription,
-                    maxLines: descriptionMaxLine,
+                      const SizedBox(width: 4),
+                      Expanded(
+                        child: DisplayNameCard(
+                          credentialModel: credentialModel,
+                          style: Theme.of(context).textTheme.credentialTitle,
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
+                  const SizedBox(height: 5),
+                  Expanded(
+                    child: DisplayDescriptionCard(
+                      credentialModel: credentialModel,
+                      style: Theme.of(context).textTheme.credentialDescription,
+                      maxLines: descriptionMaxLine,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 1,
-          child: DisplayIssuer(
-            issuer: credentialModel
-                .credentialPreview.credentialSubjectModel.issuedBy!,
-          ),
-        )
-      ],
+          Expanded(
+            flex: 1,
+            child: DisplayIssuer(
+              issuer: credentialModel
+                  .credentialPreview.credentialSubjectModel.issuedBy!,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
