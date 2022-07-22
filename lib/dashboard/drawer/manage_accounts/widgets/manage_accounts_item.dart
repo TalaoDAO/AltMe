6f251d91fd4790f1d7d96ec055222333fc89d8bc
TalebRafiepour <taleb_r75@yahoo.com>
2022-07-22 16:49:42 +0430

    show alert and ask for pinCode before show private key of account

--- lib/dashboard/drawer/manage_accounts/widgets/manage_accounts_item.dart
+++ lib/dashboard/drawer/manage_accounts/widgets/manage_accounts_item.dart
@@ -2,6 +2,7 @@ import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/dashboard.dart';
 import 'package:altme/dashboard/drawer/manage_accounts/view/account_private_key_page.dart';
 import 'package:altme/l10n/l10n.dart';
+import 'package:altme/pin_code/pin_code.dart';
 import 'package:altme/theme/theme.dart';
 import 'package:altme/wallet/model/model.dart';
 import 'package:flutter/material.dart';
@@ -103,10 +104,22 @@ class ManageAccountsItem extends StatelessWidget {
               ),
               RevealPrivateKeyButton(
                 onTap: () {
-                  Navigator.of(context).push<void>(
-                    AccountPrivateKeyPage.route(
-                      privateKey: cryptoAccountData.secretKey,
-                    ),
+                  AccountPrivateKeyDialog.show(
+                    context: context,
+                    onContinueClick: () {
+                      Navigator.of(context).push<void>(
+                        PinCodePage.route(
+                          restrictToBack: false,
+                          isValidCallback: () {
+                            Navigator.of(context).push<void>(
+                              AccountPrivateKeyPage.route(
+                                privateKey: cryptoAccountData.secretKey,
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
                   );
                 },
               ),
--- lib/dashboard/drawer/manage_accounts/widgets/widgets.dart
+++ lib/dashboard/drawer/manage_accounts/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'account_private_key_dialog.dart';
 export 'manage_accounts_item.dart';
 export 'reveal_private_key_button.dart';
 export 'see_address_button.dart';
