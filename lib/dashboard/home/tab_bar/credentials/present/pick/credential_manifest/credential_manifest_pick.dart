75331fd15dcec9e813f31ad774caf9602d3dd73c
Bibash Shrestha <bibashshrestha@gmail.com>
2022-08-16 19:39:07 +0545

    remove page

--- lib/dashboard/home/tab_bar/credentials/present/pick/credential_manifest/credential_manifest_pick.dart
+++ lib/dashboard/home/tab_bar/credentials/present/pick/credential_manifest/credential_manifest_pick.dart
@@ -2,4 +2,3 @@ export 'cubit/credential_manifest_pick_cubit.dart';
 export 'helpers/get_credentials_from_filter_list.dart';
 export 'helpers/get_credentials_from_presentation_definition.dart';
 export 'view/credential_manifest_credential_offer_pick_page.dart';
-export 'view/credential_manifest_presentation_pick_page.dart';
