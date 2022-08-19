defc8daa0a38773f93ff5330e5927a77089c1798
hawkbee1 <googandads@gmail.com>
2022-08-19 14:45:06 +0200

    Replace mobile_scanner by flutter_barcode_scanner

--- android/app/src/main/kotlin/co/talao/alt/me/MainActivity.kt
+++ android/app/src/main/kotlin/co/talao/alt/me/MainActivity.kt
@@ -1,6 +1,6 @@
 package co.altme.alt.me.altme
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
