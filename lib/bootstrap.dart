13a6bb70aa053ae8d95877360428c0ff363308f5
hawkbee1 <googandads@gmail.com>
2022-09-03 16:15:39 +0200

    Turn workmanager in production mode + update to version: 1.1.5+53

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -119,7 +119,7 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   await Workmanager().initialize(
     callbackDispatcher, // The top level function, aka callbackDispatcher
     isInDebugMode:
-        true, // If enabled it will post a notification whenever the task is
+        false, // If enabled it will post a notification whenever the task is
     // running. Handy for debugging tasks
   );
 
