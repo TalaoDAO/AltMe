bc93891b49e7bb0c456f1b5b5f3740e92b90fd6d
TalebRafiepour <taleb_r75@yahoo.com>
2022-09-16 15:00:43 +0430

    update ageRangeCard to use base card

--- lib/dashboard/home/tab_bar/credentials/widgets/credential_widget/age_range_widget.dart
+++ lib/dashboard/home/tab_bar/credentials/widgets/credential_widget/age_range_widget.dart
@@ -1,7 +1,6 @@
 import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/dashboard.dart';
-import 'package:altme/l10n/l10n.dart';
-import 'package:altme/theme/theme.dart';
+import 'package:altme/dashboard/home/tab_bar/credentials/widgets/credential_widget/identity_credential_base_widget.dart';
 import 'package:flutter/material.dart';
 
 class AgeRangeDisplayInList extends StatelessWidget {
@@ -57,163 +56,25 @@ class AgeRangeRecto extends Recto {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     final ageRangeModel = credentialModel
         .credentialPreview.credentialSubjectModel as AgeRangeModel;
 
-    return CredentialImage(
-      image: ImageStrings.ageRangeProof,
-      child: AspectRatio(
-        aspectRatio: Sizes.credentialAspectRatio,
-        child: CustomMultiChildLayout(
-          delegate: AgeRangeRectoDelegate(position: Offset.zero),
-          children: [
-            LayoutId(
-              id: 'provided-by',
-              child: FractionallySizedBox(
-                widthFactor: 0.75,
-                heightFactor: 0.15,
-                child: MyRichText(
-                  text: TextSpan(
-                    text: '${l10n.providedBy} ',
-                    style: Theme.of(context).textTheme.subMessage,
-                    children: [
-                      TextSpan(
-                        text: credentialModel.credentialPreview
-                            .credentialSubjectModel.issuedBy?.name,
-                        style: Theme.of(context).textTheme.title,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            LayoutId(
-              id: 'age-range',
-              child: FractionallySizedBox(
-                widthFactor: 0.65,
-                heightFactor: 0.15,
-                child: MyText(
-                  (ageRangeModel.ageRange != null &&
-                          ageRangeModel.ageRange!.isNotEmpty)
-                      ? '${ageRangeModel.ageRange} YO'
-                      : '--',
-                  style: Theme.of(context).textTheme.title,
-                ),
-              ),
-            ),
-            LayoutId(
-              id: 'issued-on',
-              child: FractionallySizedBox(
-                heightFactor: 0.12,
-                widthFactor: 0.4,
-                child: MyText(
-                  l10n.issuedOn,
-                  style: Theme.of(context).textTheme.subMessage,
-                ),
-              ),
-            ),
-            LayoutId(
-              id: 'issued-on-value',
-              child: FractionallySizedBox(
-                heightFactor: 0.12,
-                widthFactor: 0.4,
-                child: MyText(
-                  UiDate.formatDateForCredentialCard(
-                    credentialModel.credentialPreview.issuanceDate,
-                  ),
-                  style: Theme.of(context).textTheme.title,
-                ),
-              ),
-            ),
-            LayoutId(
-              id: 'expiration-date',
-              child: FractionallySizedBox(
-                heightFactor: 0.12,
-                widthFactor: 0.4,
-                child: MyText(
-                  l10n.expirationDate,
-                  style: Theme.of(context).textTheme.subMessage,
-                ),
-              ),
-            ),
-            LayoutId(
-              id: 'expiration-date-value',
-              child: FractionallySizedBox(
-                heightFactor: 0.12,
-                widthFactor: 0.4,
-                child: MyText(
-                  credentialModel.expirationDate == null
-                      ? '--'
-                      : UiDate.formatDateForCredentialCard(
-                          credentialModel.expirationDate!,
-                        ),
-                  style: Theme.of(context).textTheme.title,
-                ),
-              ),
-            ),
-          ],
-        ),
+    return IdentityCredentialBaseWidget(
+      cardBackgroundImagePath: ImageStrings.ageRangeProof,
+      issuerName: credentialModel
+          .credentialPreview.credentialSubjectModel.issuedBy?.name,
+      value:
+          (ageRangeModel.ageRange != null && ageRangeModel.ageRange!.isNotEmpty)
+              ? '${ageRangeModel.ageRange} YO'
+              : '--',
+      issuanceDate: UiDate.formatDateForCredentialCard(
+        credentialModel.credentialPreview.issuanceDate,
       ),
+      expirationDate: credentialModel.expirationDate == null
+          ? '--'
+          : UiDate.formatDateForCredentialCard(
+              credentialModel.expirationDate!,
+            ),
     );
-  }
-}
-
-class AgeRangeRectoDelegate extends MultiChildLayoutDelegate {
-  AgeRangeRectoDelegate({this.position = Offset.zero});
-
-  final Offset position;
-
-  @override
-  void performLayout(Size size) {
-    if (hasChild('provided-by')) {
-      layoutChild('provided-by', BoxConstraints.loose(size));
-      positionChild(
-        'provided-by',
-        Offset(size.width * 0.06, size.height * 0.27),
-      );
-    }
-
-    if (hasChild('age-range')) {
-      layoutChild('age-range', BoxConstraints.loose(size));
-      positionChild(
-        'age-range',
-        Offset(size.width * 0.06, size.height * 0.50),
-      );
-    }
-
-    if (hasChild('issued-on')) {
-      layoutChild('issued-on', BoxConstraints.loose(size));
-      positionChild(
-        'issued-on',
-        Offset(size.width * 0.06, size.height * 0.70),
-      );
-    }
-    if (hasChild('issued-on-value')) {
-      layoutChild('issued-on-value', BoxConstraints.loose(size));
-      positionChild(
-        'issued-on-value',
-        Offset(size.width * 0.06, size.height * 0.82),
-      );
-    }
-    if (hasChild('expiration-date')) {
-      layoutChild('expiration-date', BoxConstraints.loose(size));
-      positionChild(
-        'expiration-date',
-        Offset(size.width * 0.5, size.height * 0.70),
-      );
-    }
-    if (hasChild('expiration-date-value')) {
-      layoutChild('expiration-date-value', BoxConstraints.loose(size));
-      positionChild(
-        'expiration-date-value',
-        Offset(size.width * 0.5, size.height * 0.82),
-      );
-    }
-  }
-
-  @override
-  bool shouldRelayout(AgeRangeRectoDelegate oldDelegate) {
-    return oldDelegate.position != position;
   }
 }
