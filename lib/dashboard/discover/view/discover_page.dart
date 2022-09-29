e72984e3053f8781faef57a82523f16fc842e0f6
Bibash Shrestha <bibashshrestha@gmail.com>
2022-09-29 10:17:58 +0545

    little padding in discover page

--- lib/dashboard/discover/view/discover_page.dart
+++ lib/dashboard/discover/view/discover_page.dart
@@ -2,6 +2,7 @@ import 'package:altme/app/shared/alert_message/alert_message.dart';
 import 'package:altme/app/shared/enum/status/app_status.dart';
 import 'package:altme/app/shared/loading/loading_view.dart';
 import 'package:altme/app/shared/widget/base/page.dart';
+import 'package:altme/app/shared/widget/widget.dart';
 import 'package:altme/dashboard/home/tab_bar/tab_bar.dart';
 import 'package:altme/theme/theme.dart';
 import 'package:altme/wallet/wallet.dart';
@@ -53,24 +54,27 @@ class _DiscoverPageState extends State<DiscoverPage> {
         builder: (context, state) {
           final CredentialListCubit credentialListCubit =
               context.read<CredentialListCubit>();
-          return DiscoverCredentialList(
-            onRefresh: () async {
-              await context
-                  .read<CredentialListCubit>()
-                  .initialise(context.read<WalletCubit>());
-            },
-            state: state.populate(
-              gamingCredentials: credentialListCubit.dummyListFromCategory(
-                state.gamingCategories,
+          return BackgroundCard(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: DiscoverCredentialList(
+              onRefresh: () async {
+                await context
+                    .read<CredentialListCubit>()
+                    .initialise(context.read<WalletCubit>());
+              },
+              state: state.populate(
+                gamingCredentials: credentialListCubit.dummyListFromCategory(
+                  state.gamingCategories,
+                ),
+                communityCredentials: credentialListCubit.dummyListFromCategory(
+                  state.communityCategories,
+                ),
+                identityCredentials: credentialListCubit.dummyListFromCategory(
+                  state.identityCategories,
+                ),
+                proofOfOwnershipCredentials: [],
+                othersCredentials: [],
               ),
-              communityCredentials: credentialListCubit.dummyListFromCategory(
-                state.communityCategories,
-              ),
-              identityCredentials: credentialListCubit.dummyListFromCategory(
-                state.identityCategories,
-              ),
-              proofOfOwnershipCredentials: [],
-              othersCredentials: [],
             ),
           );
         },
