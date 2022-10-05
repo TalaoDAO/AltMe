c9273bf6da16c17fae17c67012cf55796f031646
hawkbee1 <googandads@gmail.com>
2022-10-05 11:59:56 +0200

    Add main.dart from ios branch

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:arago_wallet/app/app.dart';
+import 'package:arago_wallet/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App(flavorMode: FlavorMode.production));
+}
