7560ffa3224e895e7f9fc65969b285a767ab4043
TalebRafiepour <taleb_r75@yahoo.com>
2022-10-14 17:16:37 +0330

    see transaction detials

--- lib/dashboard/home/tab_bar/tokens/send_receive_home/widgets/transaction_item.dart
+++ lib/dashboard/home/tab_bar/tokens/send_receive_home/widgets/transaction_item.dart
@@ -28,96 +28,113 @@ class TransactionItem extends StatelessWidget {
       value: operationModel.parameter?.value?.value ??
           operationModel.amount.toString(),
     );
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(
-          height: Sizes.spaceXSmall,
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              operationModel.formatedDateTime,
-              style: Theme.of(context).textTheme.caption2,
-            ),
-            Text(
-              tokenUsdPrice != null
-                  ? (tokenUsdPrice! * amount)
-                          .toStringAsFixed(2)
-                          .formatNumber() +
-                      r'$'
-                  : r'$--.--',
-              style: Theme.of(context).textTheme.caption2,
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: Sizes.spaceXSmall,
-        ),
-        Row(
-          children: [
-            Image.asset(
-              operationModel.isSender(
-                walletAddress: context
-                    .read<WalletCubit>()
-                    .state
-                    .currentAccount
-                    .walletAddress,
-              )
-                  ? IconStrings.send
-                  : IconStrings.receive,
-              width: Sizes.icon,
-              height: Sizes.icon,
-            ),
-            const SizedBox(
-              width: Sizes.space2XSmall,
-            ),
-            Text(
-              operationModel.isSender(
-                walletAddress: context
-                    .read<WalletCubit>()
-                    .state
-                    .currentAccount
-                    .walletAddress,
-              )
-                  ? l10n.send
-                  : l10n.receive,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            const SizedBox(
-              width: Sizes.space2XSmall,
-            ),
-            Text(
-              operationModel.status,
-              style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                    color: operationModel.status.toLowerCase() == 'applied'
-                        ? Theme.of(context).colorScheme.transactionApplied
-                        : operationModel.status.toLowerCase() == 'failed' ||
-                                operationModel.status.toLowerCase() ==
-                                    'backtracked'
-                            ? Theme.of(context).colorScheme.transactionFailed
-                            : operationModel.status.toLowerCase() == 'skipped'
-                                ? Theme.of(context)
-                                    .colorScheme
-                                    .transactionSkipped
-                                : null,
-                  ),
-            ),
-            const SizedBox(width: Sizes.space2XSmall,),
-            Expanded(
-              child: MyText(
-                '${amount.toStringAsFixed(2).formatNumber()} '
-                '$symbol',
-                minFontSize: 8,
-                textAlign: TextAlign.end,
+    return InkWell(
+      onTap: () {
+        LaunchUrl.launch(
+          'https://tzkt.io/${operationModel.hash}/${operationModel.counter}',
+        );
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(
+            height: Sizes.spaceXSmall,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                operationModel.formatedDateTime,
+                style: Theme.of(context).textTheme.caption2,
+              ),
+              const Spacer(),
+              Text(
+                l10n.seeTransaction,
+                style: Theme.of(context).textTheme.caption2,
+              ),
+              const SizedBox(
+                width: Sizes.spaceSmall,
+              ),
+              Text(
+                tokenUsdPrice != null
+                    ? (tokenUsdPrice! * amount)
+                            .toStringAsFixed(2)
+                            .formatNumber() +
+                        r'$'
+                    : r'$--.--',
+                style: Theme.of(context).textTheme.caption2,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: Sizes.spaceXSmall,
+          ),
+          Row(
+            children: [
+              Image.asset(
+                operationModel.isSender(
+                  walletAddress: context
+                      .read<WalletCubit>()
+                      .state
+                      .currentAccount
+                      .walletAddress,
+                )
+                    ? IconStrings.send
+                    : IconStrings.receive,
+                width: Sizes.icon,
+                height: Sizes.icon,
+              ),
+              const SizedBox(
+                width: Sizes.space2XSmall,
+              ),
+              Text(
+                operationModel.isSender(
+                  walletAddress: context
+                      .read<WalletCubit>()
+                      .state
+                      .currentAccount
+                      .walletAddress,
+                )
+                    ? l10n.send
+                    : l10n.receive,
                 style: Theme.of(context).textTheme.headline6,
               ),
-            ),
-          ],
-        )
-      ],
+              const SizedBox(
+                width: Sizes.space2XSmall,
+              ),
+              Text(
+                operationModel.status,
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                      color: operationModel.status.toLowerCase() == 'applied'
+                          ? Theme.of(context).colorScheme.transactionApplied
+                          : operationModel.status.toLowerCase() == 'failed' ||
+                                  operationModel.status.toLowerCase() ==
+                                      'backtracked'
+                              ? Theme.of(context).colorScheme.transactionFailed
+                              : operationModel.status.toLowerCase() == 'skipped'
+                                  ? Theme.of(context)
+                                      .colorScheme
+                                      .transactionSkipped
+                                  : null,
+                    ),
+              ),
+              const SizedBox(
+                width: Sizes.space2XSmall,
+              ),
+              Expanded(
+                child: MyText(
+                  '${amount.toStringAsFixed(2).formatNumber()} '
+                  '$symbol',
+                  minFontSize: 8,
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
