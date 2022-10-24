889134762d1d7c0e3938938de46469d7543cd5b1
TalebRafiepour <taleb_r75@yahoo.com>
2022-10-24 17:19:26 +0330

    add option to change fee for beacon transactions

--- lib/dashboard/select_network_fee_bottom_sheet/cubit/select_network_fee_cubit.dart
+++ lib/dashboard/select_network_fee_bottom_sheet/cubit/select_network_fee_cubit.dart
@@ -8,8 +8,15 @@ part 'select_network_fee_cubit.g.dart';
 part 'select_network_fee_state.dart';
 
 class SelectNetworkFeeCubit extends Cubit<SelectNetworkFeeState> {
-  SelectNetworkFeeCubit({required NetworkFeeModel selectedNetworkFee})
-      : super(SelectNetworkFeeState(selectedNetworkFee: selectedNetworkFee));
+  SelectNetworkFeeCubit({
+    required NetworkFeeModel selectedNetworkFee,
+    List<NetworkFeeModel>? networkFeeList,
+  }) : super(
+          SelectNetworkFeeState(
+            selectedNetworkFee: selectedNetworkFee,
+            networkFeeList: networkFeeList,
+          ),
+        );
 
   void setSelectedNetworkFee({required NetworkFeeModel selectedNetworkFee}) {
     emit(state.copyWith(selectedNetworkFee: selectedNetworkFee));
