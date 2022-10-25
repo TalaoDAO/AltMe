dbf38be20c6703e1c2f1c53a8b695e7633e90045
hawkbee1 <googandads@gmail.com>
2022-10-25 11:42:19 +0200

    build 84, credentialOffer loops on select credential #914

--- lib/app/shared/alert_message/alert_message.dart
+++ lib/app/shared/alert_message/alert_message.dart
@@ -1,6 +1,5 @@
 import 'package:altme/app/app.dart';
 import 'package:altme/l10n/l10n.dart';
-import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 class AlertMessage {
@@ -10,21 +9,16 @@ class AlertMessage {
   }) {
     final MessageHandler messageHandler = stateMessage.messageHandler!;
     final String message = messageHandler.getMessage(context, messageHandler);
-    showTopSnackBar(
-      context: context,
-      message: message,
-      iconPath: stateMessage.type!.iconPath,
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(milliseconds: 700),
+        content: SnackBarContent(
+          message: message,
+          iconPath: stateMessage.type!.iconPath,
+        ),
+        backgroundColor: Colors.transparent,
+      ),
     );
-
-    // ScaffoldMessenger.of(context).showSnackBar(
-    //   SnackBar(
-    //     content: SnackBarContent(
-    //       message: message,
-    //       iconPath: stateMessage.type!.iconPath,
-    //     ),
-    //     backgroundColor: Colors.transparent,
-    //   ),
-    // );
   }
 
   static void showStringMessage({
@@ -32,77 +26,16 @@ class AlertMessage {
     required String message,
     required MessageType messageType,
   }) {
-    showTopSnackBar(
-      context: context,
-      message: message,
-      iconPath: messageType.iconPath,
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(milliseconds: 700),
+        content: SnackBarContent(
+          message: message,
+          iconPath: messageType.iconPath,
+        ),
+        backgroundColor: Colors.transparent,
+      ),
     );
-
-    // ScaffoldMessenger.of(context).showSnackBar(
-    //   SnackBar(
-    //     content: SnackBarContent(
-    //       message: message,
-    //       iconPath: messageType.iconPath,
-    //     ),
-    //     backgroundColor: Colors.transparent,
-    //   ),
-    // );
-  }
-
-  static void showTopSnackBar({
-    required BuildContext context,
-    required String message,
-    required String iconPath,
-  }) {
-    Flushbar<bool>? flush;
-    final l10n = context.l10n;
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      flush = Flushbar<bool>(
-        backgroundColor: Colors.white,
-        flushbarPosition: FlushbarPosition.TOP,
-        flushbarStyle: FlushbarStyle.FLOATING,
-        reverseAnimationCurve: Curves.decelerate,
-        duration: const Duration(seconds: 3),
-        messageColor: Colors.black,
-        messageText: MyText(
-          message,
-          style: Theme.of(context)
-              .textTheme
-              .bodySmall
-              ?.copyWith(color: Colors.black, fontSize: 12),
-          maxLines: 2,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(Sizes.smallRadius),
-        ),
-        padding: const EdgeInsets.symmetric(vertical: Sizes.spaceSmall),
-        margin: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        icon: Image.asset(
-          iconPath,
-          width: Sizes.icon,
-          height: Sizes.icon,
-        ),
-        mainButton: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.zero,
-            backgroundColor: Colors.transparent,
-            shadowColor: Colors.transparent,
-          ),
-          onPressed: () async {
-            await flush!.dismiss(true);
-          },
-          child: Text(
-            l10n.close.toUpperCase(),
-            style: Theme.of(context).textTheme.button?.copyWith(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-        ),
-      )..show(context);
-    });
   }
 }
 
