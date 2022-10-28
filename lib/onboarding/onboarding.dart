69996a79a1e61ee3755af9949ae368008d249802
TalebRafiepour <taleb_r75@yahoo.com>
2022-10-28 13:37:37 +0330

    create starter page of onboarding, create,import

--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -1,6 +1,7 @@
 export 'first/onboarding_first.dart';
 export 'gen_phrase/onboarding_gen_phrase.dart';
 export 'second/onboarding_second.dart';
+export 'starter/starter.dart';
 export 'submit_enterprise_user/submit_enterprise_user.dart';
 export 'third/onboarding_third.dart';
 export 'tos/onboarding_terms.dart';
---
+++ lib/onboarding/starter/view/starter_page.dart
@@ -0,0 +1,87 @@
+import 'package:altme/app/app.dart';
+import 'package:altme/l10n/l10n.dart';
+import 'package:altme/onboarding/onboarding.dart';
+import 'package:altme/splash/splash.dart';
+import 'package:altme/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class StarterPage extends StatelessWidget {
+  const StarterPage({super.key});
+
+  static Route route() {
+    return MaterialPageRoute<void>(
+      settings: const RouteSettings(name: '/starterPage'),
+      builder: (_) => const StarterPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return BasePage(
+      scrollView: false,
+      padding: EdgeInsets.zero,
+      useSafeArea: false,
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.darkGradientStartColor,
+              Theme.of(context).colorScheme.darkGradientEndColor,
+            ],
+            end: Alignment.topCenter,
+            begin: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.all(Sizes.spaceLarge),
+              child: TitleText(),
+            ),
+            const SubTitle(),
+            const Spacer(
+              flex: 2,
+            ),
+            const SplashImage(),
+            const Spacer(
+              flex: 2,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).pushAndRemoveUntil<void>(
+                  // TODO(Taleb): here must navigate to pin code creation page
+                  OnBoardingSecondPage.route(),
+                  (Route<dynamic> route) => route.isFirst,
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(Sizes.spaceLarge),
+                child: GradientButtonText(
+                  text: l10n.import_wallet,
+                  onPressed: () {},
+                  fontSize: 18,
+                  upperCase: true,
+                ),
+              ),
+            ),
+            MyGradientButton(
+              text: l10n.create_wallet,
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil<void>(
+                  // TODO(Taleb): here must navigate to pin code creation page
+                  OnBoardingSecondPage.route(),
+                  (Route<dynamic> route) => route.isFirst,
+                );
+              },
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
