109fc9a334fcbc3d142988de0777d9e1c73f9d79
TalebRafiepour <taleb_r75@yahoo.com>
2022-10-31 18:01:53 +0330

    minor fix

--- lib/onboarding/wallet_ready/view/wallet_ready_page.dart
+++ lib/onboarding/wallet_ready/view/wallet_ready_page.dart
@@ -2,7 +2,6 @@ import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/dashboard.dart';
 import 'package:altme/l10n/l10n.dart';
 import 'package:altme/onboarding/onboarding.dart';
-import 'package:altme/theme/theme.dart';
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
