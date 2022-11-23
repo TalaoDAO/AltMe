570aeb18789cfc077eecb29ab8db609fe3678d61
hawkbee1 <googandads@gmail.com>
2022-11-23 17:26:27 +0100

    Issue with NFT display grey #957

--- lib/app/shared/widget/cached_image_from_network.dart
+++ lib/app/shared/widget/cached_image_from_network.dart
@@ -48,39 +48,54 @@ class CachedImageFromNetwork extends StatelessWidget {
                         margin: const EdgeInsets.all(10),
                         child: Text(downloadProgress.progress.toString()),
                       )
-                    : Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Theme.of(context).colorScheme.primary,
-                              Theme.of(context).colorScheme.secondary,
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                            stops: const [0.3, 1.0],
-                          ),
-                        ),
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(25),
-                            child: Text(
-                              errorMessage!,
-                              style:
-                                  Theme.of(context).textTheme.cacheErrorMessage,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      );
+                    : ErrorWidget(errorMessage: errorMessage);
               },
-              errorWidget: (context, error, dynamic _) => Container(
-                color: Theme.of(context).colorScheme.lightGrey,
-                child: Icon(
-                  Icons.error,
-                  color: Theme.of(context).colorScheme.darkGrey,
-                ),
-              ),
+              errorWidget: (context, error, dynamic _) => errorMessage == null
+                  ? Container(
+                      color: Theme.of(context).colorScheme.lightGrey,
+                      child: Icon(
+                        Icons.error,
+                        color: Theme.of(context).colorScheme.darkGrey,
+                      ),
+                    )
+                  : ErrorWidget(errorMessage: errorMessage),
             ),
+    );
+  }
+}
+
+class ErrorWidget extends StatelessWidget {
+  const ErrorWidget({
+    Key? key,
+    required this.errorMessage,
+  }) : super(key: key);
+
+  final String? errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).colorScheme.primary,
+            Theme.of(context).colorScheme.secondary,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          stops: const [0.3, 1.0],
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Text(
+            errorMessage!,
+            style: Theme.of(context).textTheme.cacheErrorMessage,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
