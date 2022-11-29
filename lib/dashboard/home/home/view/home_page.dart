28987ec95025ae01b57ffc9a8f369064a6bca03e
TalebRafiepour <taleb_r75@yahoo.com>
2022-11-29 17:54:15 +0330

    update app logo and hide NFT and Crypto part

--- lib/dashboard/home/home/view/home_page.dart
+++ lib/dashboard/home/home/view/home_page.dart
@@ -35,7 +35,8 @@ class HomePage extends StatelessWidget {
       },
       // TODO(all): Remove IosTabControllerPage when apple accept our NFT #664, https://github.com/TalaoDAO/AltMe/issues/664
       // Setting to hide gallery when on ios
-      child: const TabControllerPage(),
+      // child: const TabControllerPage(),
+      child: const CredentialsListPage(),
     );
   }
 }
