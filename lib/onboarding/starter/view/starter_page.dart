ace8db13588435c8d72f9db1fa53bcb044f6f52a
Bibash Shrestha <bibashshrestha@gmail.com>
2022-12-16 10:27:11 +0545

    splash screen and starter page responsive to all screens

--- lib/onboarding/starter/view/starter_page.dart
+++ lib/onboarding/starter/view/starter_page.dart
@@ -25,7 +25,6 @@ class StarterPage extends StatelessWidget {
       },
       child: Scaffold(
         body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: Sizes.spaceSmall),
           alignment: Alignment.center,
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -37,72 +36,70 @@ class StarterPage extends StatelessWidget {
               begin: Alignment.bottomCenter,
             ),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              const SizedBox(
-                height: Sizes.space2XLarge,
-              ),
-              const Spacer(),
-              const TitleText(),
-              const SizedBox(
-                height: Sizes.spaceNormal,
-              ),
-              const SubTitle(),
-              const Spacer(
-                flex: 2,
-              ),
-              const SplashImage(),
-              const Spacer(
-                flex: 2,
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).push<void>(
-                    EnterNewPinCodePage.route(
-                      isFromOnboarding: true,
-                      isValidCallback: () {
-                        Navigator.of(context).pushReplacement<void,void>(
-                          ActiviateBiometricsPage.route(
-                            routeType: WalletRouteType.recover,
-                          ),
-                        );
-                      },
-                      restrictToBack: false,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Spacer(flex: 1),
+                  const TitleText(),
+                  const Spacer(flex: 1),
+                  const SubTitle(),
+                  const Spacer(flex: 3),
+                  const SplashImage(),
+                  const Spacer(flex: 2),
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        EnterNewPinCodePage.route(
+                          isFromOnboarding: true,
+                          isValidCallback: () {
+                            Navigator.of(context).pushReplacement<void, void>(
+                              ActiviateBiometricsPage.route(
+                                routeType: WalletRouteType.recover,
+                              ),
+                            );
+                          },
+                          restrictToBack: false,
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(Sizes.spaceLarge),
+                      child: GradientButtonText(
+                        text: l10n.import_wallet,
+                        onPressed: () {},
+                        fontSize: 18,
+                        upperCase: true,
+                      ),
                     ),
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(Sizes.spaceLarge),
-                  child: GradientButtonText(
-                    text: l10n.import_wallet,
-                    onPressed: () {},
-                    fontSize: 18,
-                    upperCase: true,
                   ),
-                ),
+                  MyGradientButton(
+                    text: l10n.create_wallet,
+                    onPressed: () {
+                      Navigator.of(context).push<void>(
+                        EnterNewPinCodePage.route(
+                          isFromOnboarding: true,
+                          isValidCallback: () {
+                            Navigator.of(context).pushReplacement<void, void>(
+                              ActiviateBiometricsPage.route(
+                                routeType: WalletRouteType.create,
+                              ),
+                            );
+                          },
+                          restrictToBack: false,
+                        ),
+                      );
+                    },
+                  ),
+                  const Spacer(),
+                  const SizedBox(
+                    height: Sizes.spaceSmall,
+                  ),
+                ],
               ),
-              MyGradientButton(
-                text: l10n.create_wallet,
-                onPressed: () {
-                  Navigator.of(context).push<void>(
-                    EnterNewPinCodePage.route(
-                      isFromOnboarding: true,
-                      isValidCallback: () {
-                        Navigator.of(context).pushReplacement<void,void>(
-                          ActiviateBiometricsPage.route(
-                            routeType: WalletRouteType.create,
-                          ),
-                        );
-                      },
-                      restrictToBack: false,
-                    ),
-                  );
-                },
-              ),
-              const Spacer(),
-              const SizedBox(height: Sizes.spaceSmall,),
-            ],
+            ),
           ),
         ),
       ),
