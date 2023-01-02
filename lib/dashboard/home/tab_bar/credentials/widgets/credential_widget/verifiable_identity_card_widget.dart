bc6a859f304cb01b2589d78511ebde632c6cd3c4
Bibash Shrestha <bibashshrestha@gmail.com>
2023-01-02 19:17:05 +0545

    remove name from verfiablediploma and verifiableId card

--- lib/dashboard/home/tab_bar/credentials/widgets/credential_widget/verifiable_identity_card_widget.dart
+++ lib/dashboard/home/tab_bar/credentials/widgets/credential_widget/verifiable_identity_card_widget.dart
@@ -54,15 +54,14 @@ class VerifiableIdCardRecto extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final verifiableIdCardModel = credentialModel
-        .credentialPreview.credentialSubjectModel as VerifiableIdCardModel;
+    // final verifiableIdCardModel = credentialModel
+    //     .credentialPreview.credentialSubjectModel as VerifiableIdCardModel;
 
     return CredentialBaseWidget(
       cardBackgroundImagePath: ImageStrings.verifiableIdCard,
       issuerName: credentialModel
           .credentialPreview.credentialSubjectModel.issuedBy?.name,
-      value: '${verifiableIdCardModel.givenName}'
-          ' ${verifiableIdCardModel.familyName}',
+      value: '',
       issuanceDate: UiDate.formatDateForCredentialCard(
         credentialModel.credentialPreview.issuanceDate,
       ),
