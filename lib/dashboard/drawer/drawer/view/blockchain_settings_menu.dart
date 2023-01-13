366f8dc24cdc9348b6e151e4f8bef5ffa42336eb
Bibash Shrestha <bibashshrestha@gmail.com>
2023-01-13 20:56:28 +0530

    wallet phrase in wallet security #1238

--- lib/dashboard/drawer/drawer/view/blockchain_settings_menu.dart
+++ lib/dashboard/drawer/drawer/view/blockchain_settings_menu.dart
@@ -1,12 +1,10 @@
 import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/dashboard.dart';
 import 'package:altme/l10n/l10n.dart';
-import 'package:altme/pin_code/pin_code.dart';
 import 'package:altme/theme/theme.dart';
 import 'package:altme/wallet/wallet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:secure_storage/secure_storage.dart';
 
 class BlockchainSettingsMenu extends StatelessWidget {
   const BlockchainSettingsMenu({super.key});
@@ -26,28 +24,6 @@ class BlockchainSettingsMenu extends StatelessWidget {
 
 class BlockchainSettingsView extends StatelessWidget {
   const BlockchainSettingsView({super.key});
-
-  //method for set new pin code
-  Future<void> setNewPinCode(
-    BuildContext context,
-    AppLocalizations l10n,
-  ) async {
-    Navigator.of(context).pop();
-    await Navigator.of(context).push<void>(
-      EnterNewPinCodePage.route(
-        isFromOnboarding: false,
-        isValidCallback: () {
-          Navigator.of(context).pop();
-          AlertMessage.showStateMessage(
-            context: context,
-            stateMessage: StateMessage.success(
-              stringMessage: l10n.yourPinCodeChangedSuccessfully,
-            ),
-          );
-        },
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,37 +52,6 @@ class BlockchainSettingsView extends StatelessWidget {
                   onTap: () {
                     Navigator.of(context)
                         .push<void>(ManageAccountsPage.route());
-                  },
-                ),
-                DrawerItem(
-                  title: l10n.showWalletRecoveryPhrase,
-                  onTap: () async {
-                    final confirm = await showDialog<bool>(
-                          context: context,
-                          builder: (context) => ConfirmDialog(
-                            title: l10n.warningDialogTitle,
-                            subtitle: l10n.warningDialogSubtitle,
-                            yes: l10n.showDialogYes,
-                            no: l10n.showDialogNo,
-                          ),
-                        ) ??
-                        false;
-
-                    if (confirm) {
-                      final pinCode =
-                          await getSecureStorage.get(SecureStorageKeys.pinCode);
-                      if (pinCode?.isEmpty ?? true) {
-                        await setNewPinCode(context, l10n);
-                      } else {
-                        await Navigator.of(context).push<void>(
-                          PinCodePage.route(
-                            isValidCallback: () => Navigator.of(context)
-                                .push<void>(RecoveryKeyPage.route()),
-                            restrictToBack: false,
-                          ),
-                        );
-                      }
-                    }
                   },
                 ),
                 DrawerItem(
