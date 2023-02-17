504c5997f36255e1c4df19e6667a88a2153e9c72
TalebRafiepour <taleb_r75@yahoo.com>
2023-02-17 19:31:44 +0330

    add some verifcation method

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_windows
   local_auth_windows
   permission_handler_windows
+  platform_device_id_windows
   secure_application
   share_plus
   url_launcher_windows
