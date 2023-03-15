ded4be63dc0db4f4ef9771ea07e0883aaed0ce90
TalebRafiepour <taleb_r75@yahoo.com>
2023-03-15 10:57:44 +0330

    some wording in help center menu #1442

--- lib/dashboard/drawer/drawer/view/help_center_menu.dart
+++ lib/dashboard/drawer/drawer/view/help_center_menu.dart
@@ -46,15 +46,25 @@ class HelpCenterView extends StatelessWidget {
                   height: Sizes.spaceSmall,
                 ),
                 DrawerItem(
-                  title: l10n.faqs,
+                  title: l10n.altmeSupport,
                   onTap: () {
-                    Navigator.of(context).push<void>(FAQsPage.route());
+                    Navigator.of(context).push<void>(
+                      AltmeSupportChatPage.route(
+                        appBarTitle: l10n.altmeSupport,
+                      ),
+                    );
                   },
                 ),
                 DrawerItem(
-                  title: '${l10n.contactUs} : ${AltMeStrings.appSupportMail}',
+                  title: '${l10n.sendAnEmail} : ${AltMeStrings.appSupportMail}',
                   onTap: () {
                     Navigator.of(context).push<void>(ContactUsPage.route());
+                  },
+                ),
+                DrawerItem(
+                  title: l10n.faqs,
+                  onTap: () {
+                    Navigator.of(context).push<void>(FAQsPage.route());
                   },
                 ),
                 DrawerItem(
@@ -82,16 +92,6 @@ class HelpCenterView extends StatelessWidget {
                       ],
                     ),
                   ),
-                ),
-                DrawerItem(
-                  title: l10n.altmeSupport,
-                  onTap: () {
-                    Navigator.of(context).push<void>(
-                      AltmeSupportChatPage.route(
-                        appBarTitle: l10n.altmeSupport,
-                      ),
-                    );
-                  },
                 ),
               ],
             ),
