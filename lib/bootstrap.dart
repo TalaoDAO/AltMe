aa2c7db11a9c9a425f3e60749ff08e3e325ce303
hawkbee1 <googandads@gmail.com>
2023-03-22 09:41:11 +0100

    remove dead code

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -13,7 +13,6 @@ import 'package:altme/dashboard/home/home/cubit/home_cubit.dart';
 import 'package:bloc/bloc.dart';
 import 'package:dartez/dartez.dart';
 import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:passbase_flutter/passbase_flutter.dart';
 import 'package:secure_storage/secure_storage.dart' as secure_storage;
 import 'package:workmanager/workmanager.dart';
@@ -113,9 +112,6 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   await PassbaseSDK.initialize(
     publishableApiKey: AltMeStrings.passBasePublishableApiKey,
   );
-
-  /// Disable Http google font
-  // GoogleFonts.config.allowRuntimeFetching = false;
 
   await Workmanager().initialize(
     callbackDispatcher, // The top level function, aka callbackDispatcher
