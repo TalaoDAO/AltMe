f4ccad6ddf0cbe6322f0e3e7f0013f73bbf5158f
hawkbee <49282360+hawkbee1@users.noreply.github.com>
2023-03-24 07:28:28 +0100

    1.12.4 (#1492)
    * Add offeredBy in json-ld context of wallet_credential (deviceInfo)
    
    * Upgrade what's new
    
    * version: 1.12.4+178
    
    * Revert "Add offeredBy in json-ld context of wallet_credential (deviceInfo)"
    
    This reverts commit 893087acae39bf22b19f4c5be477b07f5dd9796f.
    
    * OfferedBy not generated in toJson if null
    
    * bug: unlink 2 maps variables in EBSI package
    
    * version: 1.12.5+179

--- lib/dashboard/dashboard/widgets/what_is_new_dialog.dart
+++ lib/dashboard/dashboard/widgets/what_is_new_dialog.dart
@@ -76,10 +76,16 @@ class WhatIsNewDialog extends StatelessWidget {
                             NewContent(
                               version: versionNumber,
                               features: const [
-                                'Resolved Android KYC stability issues.',
+                                'Resolved wallet certificate generation issue.',
+                              ],
+                            ),
+                            const NewContent(
+                              version: '1.12.3',
+                              features: [
+                                'Resolved KYC stability issues.',
                                 'Integrated live chat support for partners with loyalty cards.',
                                 'Expanded "Discover" section with the addition of Bloometa card.',
-                                'Enhanced credential backup reliability on some Android devices.',
+                                'Enhanced credential backup reliability on some devices.',
                                 'Optimized help center layout and user experience.',
                               ],
                             ),
