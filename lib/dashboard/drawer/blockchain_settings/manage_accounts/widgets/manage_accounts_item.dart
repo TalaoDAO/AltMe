bb543a16d77345434018376e51f400a3363b375d
Bibash Shrestha <bibashshrestha@gmail.com>
2023-04-10 16:09:02 +0530

    linter update

--- lib/dashboard/drawer/blockchain_settings/manage_accounts/widgets/manage_accounts_item.dart
+++ lib/dashboard/drawer/blockchain_settings/manage_accounts/widgets/manage_accounts_item.dart
@@ -1,6 +1,5 @@
 import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/dashboard.dart';
-import 'package:altme/dashboard/drawer/blockchain_settings/manage_accounts/view/account_private_key_page.dart';
 import 'package:altme/l10n/l10n.dart';
 import 'package:altme/pin_code/pin_code.dart';
 import 'package:altme/theme/theme.dart';
