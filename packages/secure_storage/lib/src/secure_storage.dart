a8d5521b054dd132018c7102560ddc9be05f21a0
hawkbee1 <googandads@gmail.com>
2023-05-12 15:51:47 +0200

    Android 1.14.3 crashes on Hugo device after splash screen) #1632

--- packages/secure_storage/lib/src/secure_storage.dart
+++ packages/secure_storage/lib/src/secure_storage.dart
@@ -1,8 +1,42 @@
+import 'dart:io';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 ///SecureStorageProvider getter
-SecureStorageProvider get getSecureStorage =>
-    const SecureStorageProvider(FlutterSecureStorage());
+SecureStorageProvider get getSecureStorage {
+  if (Platform.isAndroid) {
+    try {
+      /// some android devices need additional parameters to use secureStorage
+      /// We are testing getAllValues to check if those additional settings are
+      /// required. We don't set it by default because old wallets would loose
+      /// their data.
+      /// https://github.com/mogol/flutter_secure_storage/issues/354
+
+      const secureStorage = SecureStorageProvider(FlutterSecureStorage());
+      // ignore: cascade_invocations
+      secureStorage.getAllValues();
+      return secureStorage;
+    } catch (e) {
+      const defaultAndroidOptions = AndroidOptions(
+        encryptedSharedPreferences: true,
+        sharedPreferencesName: 'altme',
+      );
+      return const SecureStorageProvider(
+        FlutterSecureStorage(
+          aOptions: defaultAndroidOptions,
+        ),
+      );
+    }
+  }
+  const defaultIOSOptions =
+      IOSOptions(accessibility: KeychainAccessibility.first_unlock);
+
+  return const SecureStorageProvider(
+    FlutterSecureStorage(
+      iOptions: defaultIOSOptions,
+    ),
+  );
+}
 
 ///SecureStorageProvider
 class SecureStorageProvider {
@@ -11,21 +45,11 @@ class SecureStorageProvider {
 
   final FlutterSecureStorage _storage;
 
-  IOSOptions get _defaultIOSOptions =>
-      const IOSOptions(accessibility: KeychainAccessibility.first_unlock);
-
-  AndroidOptions get _defaultAndroidOptions => const AndroidOptions(
-        encryptedSharedPreferences: true,
-        sharedPreferencesName: 'altme',
-      );
-
   ///get
   Future<String?> get(String key) async {
     try {
       return await _storage.read(
         key: key,
-        iOptions: _defaultIOSOptions,
-        aOptions: _defaultAndroidOptions,
       );
     } catch (e) {
       return null;
@@ -37,8 +61,6 @@ class SecureStorageProvider {
     return _storage.write(
       key: key,
       value: val,
-      iOptions: _defaultIOSOptions,
-      aOptions: _defaultAndroidOptions,
     );
   }
 
@@ -46,25 +68,17 @@ class SecureStorageProvider {
   Future<void> delete(String key) async {
     return _storage.delete(
       key: key,
-      iOptions: _defaultIOSOptions,
-      aOptions: _defaultAndroidOptions,
     );
   }
 
   ///getAllValues
   Future<Map<String, String>> getAllValues() {
-    return _storage.readAll(
-      aOptions: _defaultAndroidOptions,
-      iOptions: _defaultIOSOptions,
-    );
+    return _storage.readAll();
   }
 
   ///deleteAll
   Future<void> deleteAll() async {
-    return _storage.deleteAll(
-      aOptions: _defaultAndroidOptions,
-      iOptions: _defaultIOSOptions,
-    );
+    return _storage.deleteAll();
   }
 
   ///deleteAllExceptsSomeKeys
