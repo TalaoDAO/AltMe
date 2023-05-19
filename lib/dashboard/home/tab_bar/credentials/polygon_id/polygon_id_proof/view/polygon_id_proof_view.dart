9385640eda87c1b4073fd47f7d8061df4535a6d3
Bibash Shrestha <bibashshrestha@gmail.com>
2023-05-19 12:43:08 +0545

    word update #1628

--- lib/dashboard/home/tab_bar/credentials/polygon_id/polygon_id_proof/view/polygon_id_proof_view.dart
+++ lib/dashboard/home/tab_bar/credentials/polygon_id/polygon_id_proof/view/polygon_id_proof_view.dart
@@ -5,7 +5,6 @@ import 'package:altme/polygon_id/polygon_id.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polygonid/polygonid.dart';
-import 'package:secure_storage/secure_storage.dart';
 
 class PolygonIdProofPage extends StatelessWidget {
   const PolygonIdProofPage({
@@ -45,7 +44,7 @@ class PolygonIdProofPage extends StatelessWidget {
             ),
             SizedBox(height: 100),
             Text(
-              'CRYPTOGRPAHIC PROOF',
+              'CRYPTOGRAPHIC PROOF',
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 100),
