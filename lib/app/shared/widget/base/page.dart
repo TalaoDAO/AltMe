bfd702e439e7a2a4771d2ef8217df0b5b7e61ad3
hawkbee1 <googandads@gmail.com>
2023-08-31 17:34:55 +0200

    Revert "linter"
    This reverts commit adaef0eaedc189164b6e7b3ecaf2e1ad264494d6.

--- lib/app/shared/widget/base/page.dart
+++ lib/app/shared/widget/base/page.dart
@@ -86,8 +86,10 @@ class _BasePageState extends State<BasePage> with WidgetsBindingObserver {
           secureApplicationController.lock();
         }
 
+      case AppLifecycleState.hidden:
       case AppLifecycleState.detached:
         break;
+      // TODO(all): Handle this case.
     }
   }
 
