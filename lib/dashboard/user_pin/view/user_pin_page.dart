4ec38a1600d96f9b021759ffeb2e66b32d6b7eb7
Bibash Shrestha <bibashshrestha@gmail.com>
2023-09-15 13:11:01 +0545

    refactor: Reverted pincode flow and Fixed encoded credential presentation issue

--- lib/dashboard/user_pin/view/user_pin_page.dart
+++ lib/dashboard/user_pin/view/user_pin_page.dart
@@ -4,7 +4,6 @@ import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/dashboard.dart';
 import 'package:altme/l10n/l10n.dart';
 import 'package:altme/pin_code/pin_code.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -68,56 +67,42 @@ class _UserPinViewState extends State<UserPinView> {
     super.dispose();
   }
 
+  String? pinCodeValue;
+
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return WillPopScope(
       onWillPop: () async => false,
-      child: BlocBuilder<PinCodeViewCubit, PinCodeViewState>(
-        builder: (context, state) {
-          return BasePage(
-            backgroundColor: Theme.of(context).colorScheme.background,
-            scrollView: false,
-            body: PinCodeWidget(
-              title: l10n.pleaseInsertTheSecredCodeReceived,
-              passwordEnteredCallback: (String enterPasscode) {
-                _verificationNotifier.add(true);
-              },
-              passwordDigits: 6,
-              deleteButton: Text(
-                l10n.delete,
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              cancelButton: Text(
-                l10n.cancel,
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              cancelCallback: _onPasscodeCancelled,
-              isValidCallback: () {
-                Navigator.pop(context);
-                widget.onProceed.call(state.enteredPasscode);
-              },
-              doneButton: state.enteredPasscode.length < 4
-                  ? null
-                  : CupertinoButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        widget.onProceed.call(state.enteredPasscode);
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(24),
-                        child: Text(
-                          l10n.proceed,
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                      ),
-                    ),
-              shouldTriggerVerification: _verificationNotifier.stream,
-            ),
-          );
-        },
+      child: BasePage(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        scrollView: false,
+        body: PinCodeWidget(
+          title: l10n.pleaseInsertTheSecredCodeReceived,
+          passwordEnteredCallback: _onPasscodeEntered,
+          passwordDigits: 6,
+          deleteButton: Text(
+            l10n.delete,
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+          cancelButton: Text(
+            l10n.cancel,
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+          cancelCallback: _onPasscodeCancelled,
+          isValidCallback: () {
+            Navigator.pop(context);
+            widget.onProceed.call(pinCodeValue!);
+          },
+          shouldTriggerVerification: _verificationNotifier.stream,
+        ),
       ),
     );
+  }
+
+  Future<void> _onPasscodeEntered(String enteredPasscode) async {
+    pinCodeValue = enteredPasscode;
+    _verificationNotifier.add(true);
   }
 
   void _onPasscodeCancelled() {
