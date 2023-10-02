191fa17ad092d14d98155246225b10de9b307dd0
Bibash Shrestha <bibashshrestha@gmail.com>
2023-10-02 17:06:25 +0545

    Improve error popup #1958

--- lib/app/shared/launch_url/launch_url.dart
+++ lib/app/shared/launch_url/launch_url.dart
@@ -1,15 +1,21 @@
 import 'package:url_launcher/url_launcher.dart';
 
 class LaunchUrl {
-  static Future<void> launch(String url) async {
+  static Future<void> launch(
+    String url, {
+    LaunchMode launchMode = LaunchMode.externalApplication,
+  }) async {
     await canLaunchUrl(Uri.parse(url))
-        ? await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication)
+        ? await launchUrl(Uri.parse(url), mode: launchMode)
         : throw Exception('Could not launch $url');
   }
 
-  static Future<void> launchUri(Uri uri) async {
+  static Future<void> launchUri(
+    Uri uri, {
+    LaunchMode launchMode = LaunchMode.externalApplication,
+  }) async {
     await canLaunchUrl(uri)
-        ? await launchUrl(uri, mode: LaunchMode.externalApplication)
+        ? await launchUrl(uri, mode: launchMode)
         : throw Exception('Could not launch $uri');
   }
 }
