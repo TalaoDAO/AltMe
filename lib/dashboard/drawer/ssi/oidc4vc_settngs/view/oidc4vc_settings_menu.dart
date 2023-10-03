2b13b177e3db09021ff1d6f5b3ff0d980c5e1f17
Bibash Shrestha <bibashshrestha@gmail.com>
2023-10-03 12:16:58 +0545

    feat: merge security level and PIN digits in one screen #1956

---
+++ lib/dashboard/drawer/ssi/oidc4vc_settngs/view/oidc4vc_settings_menu.dart
@@ -0,0 +1,44 @@
+import 'package:altme/app/app.dart';
+import 'package:altme/dashboard/dashboard.dart';
+import 'package:altme/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+
+class Oidc4vcSettingMenu extends StatelessWidget {
+  const Oidc4vcSettingMenu({super.key});
+
+  static Route<dynamic> route() {
+    return MaterialPageRoute<void>(
+      settings: const RouteSettings(name: '/Oidc4vcSettingMenu'),
+      builder: (_) => const Oidc4vcSettingMenu(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Oidc4vcSettingMenuView();
+  }
+}
+
+class Oidc4vcSettingMenuView extends StatelessWidget {
+  const Oidc4vcSettingMenuView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return BasePage(
+      title: l10n.oidc4vc_settings,
+      useSafeArea: true,
+      scrollView: false,
+      titleAlignment: Alignment.topCenter,
+      padding: const EdgeInsets.symmetric(horizontal: Sizes.spaceSmall),
+      titleLeading: const BackLeadingButton(),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SecurityLevelWidget(),
+          SixOrForUserPinWidget(),
+        ],
+      ),
+    );
+  }
+}
