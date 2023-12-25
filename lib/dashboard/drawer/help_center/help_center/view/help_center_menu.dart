a112818c999d1859b7559b89f7a2ac1b97e03f91
Bibash Shrestha <bibashshrestha@gmail.com>
2023-12-25 12:57:53 +0545

    feat: Update help center options #2145

--- lib/dashboard/drawer/help_center/help_center/view/help_center_menu.dart
+++ lib/dashboard/drawer/help_center/help_center/view/help_center_menu.dart
@@ -1,8 +1,10 @@
 import 'package:altme/app/app.dart';
+import 'package:altme/dashboard/dashboard.dart';
 import 'package:altme/dashboard/drawer/drawer.dart';
 import 'package:altme/l10n/l10n.dart';
 import 'package:altme/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HelpCenterMenu extends StatelessWidget {
   const HelpCenterMenu({super.key});
@@ -26,6 +28,30 @@ class HelpCenterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+
+    final helpCenterOptions = context
+        .read<ProfileCubit>()
+        .state
+        .model
+        .profileSetting
+        .helpCenterOptions;
+
+    var email = AltMeStrings.appSupportMail;
+
+    /// set the email to custom email
+    if (helpCenterOptions.customEmailSupport &&
+        helpCenterOptions.customEmail != null) {
+      email = helpCenterOptions.customEmail!;
+    }
+
+    var customChatSupportName = l10n.support;
+
+    /// set the custom chat support
+    if (helpCenterOptions.customChatSupport &&
+        helpCenterOptions.customChatSupportName != null) {
+      customChatSupportName = helpCenterOptions.customChatSupportName!;
+    }
+
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.drawerBackground,
       child: SafeArea(
@@ -42,25 +68,32 @@ class HelpCenterView extends StatelessWidget {
                 const Center(
                   child: AltMeLogo(size: 90),
                 ),
-                const SizedBox(
-                  height: Sizes.spaceSmall,
-                ),
-                DrawerItem(
-                  title: l10n.chatWithSupport,
-                  onTap: () {
-                    Navigator.of(context).push<void>(
-                      AltmeSupportChatPage.route(
-                        appBarTitle: l10n.chatWithSupport,
-                      ),
-                    );
-                  },
-                ),
-                DrawerItem(
-                  title: l10n.sendAnEmail,
-                  onTap: () {
-                    Navigator.of(context).push<void>(ContactUsPage.route());
-                  },
-                ),
+                if (helpCenterOptions.displayChatSupport) ...[
+                  const SizedBox(height: Sizes.spaceSmall),
+                  DrawerItem(
+                    title: '${l10n.chatWith} $customChatSupportName',
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        AltmeSupportChatPage.route(
+                          appBarTitle:
+                              '${l10n.chatWith} $customChatSupportName',
+                        ),
+                      );
+                    },
+                  ),
+                ],
+                if (helpCenterOptions.displayEmailSupport) ...[
+                  DrawerItem(
+                    title: l10n.sendAnEmail,
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        ContactUsPage.route(
+                          email: email,
+                        ),
+                      );
+                    },
+                  ),
+                ],
                 DrawerItem(
                   title: l10n.faqs,
                   onTap: () {
