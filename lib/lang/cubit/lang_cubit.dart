59870fbaeff97355b54b8480656bdf23ca5505ae
hawkbee1 <googandads@gmail.com>
2024-02-29 16:59:13 +0000

    update language selection

--- lib/lang/cubit/lang_cubit.dart
+++ lib/lang/cubit/lang_cubit.dart
@@ -2,16 +2,16 @@ import 'dart:async';
 
 import 'package:altme/l10n/l10n.dart';
 import 'package:bloc/bloc.dart';
-// import 'package:devicelocale/devicelocale.dart';
+import 'package:devicelocale/devicelocale.dart';
 import 'package:flutter/material.dart';
 
 class LangCubit extends Cubit<Locale> {
   LangCubit() : super(const Locale('en', 'US'));
 
   Future<void> fetchLocale() async {
-    const Locale locale = Locale('en', 'US');
+    final Locale? locale = await Devicelocale.currentAsLocale;
 
-    final langauageCode = locale.languageCode;
+    final langauageCode = locale!.languageCode;
 
     if (AppLocalizations.supportedLocales.contains(Locale(langauageCode))) {
       setLocale(locale);
