92b4e90b24fb26d38b1a360e1a1260d0e0ee46f7
Bibash Shrestha <bibashshrestha@gmail.com>
2024-03-08 16:39:50 +0545

    feat: Emphasise verify now button over verify later #2321

--- lib/theme/app_theme/app_theme.dart
+++ lib/theme/app_theme/app_theme.dart
@@ -233,6 +233,8 @@ extension CustomColorScheme on ColorScheme {
 
   Color get deleteColor => const Color(0xff322643);
 
+  Color get blueColor => const Color(0xff322643);
+
   Color get titleColor => const Color(0xffD1CCE3);
 
   Color get valueColor => const Color(0xffFFFFFF);
