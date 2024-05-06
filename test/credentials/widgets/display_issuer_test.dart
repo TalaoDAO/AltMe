d18e3adcad22248b58f93bccf34d4a9c242c6f7c
Bibash Shrestha <bibashshrestha@gmail.com>
2024-05-06 18:19:07 +0545

    Add-update tests basic widgets

--- test/credentials/widgets/display_issuer_test.dart
+++ test/credentials/widgets/display_issuer_test.dart
@@ -5,19 +5,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('DisplayIssuer Widget work correctly', () {
-    late Author issuerWithLogo;
-    late Author issuerWithoutLogo;
     const authorName = 'Taleb';
 
-    setUp(() {
-      issuerWithLogo = const Author(authorName);
-      issuerWithoutLogo = const Author(authorName);
-    });
+    const author = Author(authorName);
 
     testWidgets('find issuer name', (WidgetTester tester) async {
       await tester.pumpWidget(
-        MaterialApp(
-          home: DisplayIssuer(issuer: issuerWithLogo),
+        const MaterialApp(
+          home: DisplayIssuer(issuer: author),
         ),
       );
       expect(find.text(authorName), findsOneWidget);
@@ -25,46 +20,24 @@ void main() {
 
     testWidgets('find issuer model', (WidgetTester tester) async {
       await tester.pumpWidget(
-        MaterialApp(
-          home: DisplayIssuer(issuer: issuerWithLogo),
+        const MaterialApp(
+          home: DisplayIssuer(issuer: author),
         ),
       );
       final displayIssuer =
           tester.widget(find.byType(DisplayIssuer)) as DisplayIssuer;
-      expect(displayIssuer.issuer, issuerWithLogo);
-    });
-
-    testWidgets('find CachedImageFromNetwork', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: DisplayIssuer(issuer: issuerWithLogo),
-        ),
-      );
-      expect(find.byType(CachedImageFromNetwork), findsOneWidget);
-    });
-
-    testWidgets('CachedImageFromNetwork gone when logo is empty',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: DisplayIssuer(issuer: issuerWithoutLogo),
-        ),
-      );
-      expect(find.byType(CachedImageFromNetwork), findsNothing);
+      expect(displayIssuer.issuer, author);
     });
 
     testWidgets('find all widget', (WidgetTester tester) async {
       await tester.pumpWidget(
-        MaterialApp(
-          home: DisplayIssuer(issuer: issuerWithLogo),
+        const MaterialApp(
+          home: DisplayIssuer(issuer: author),
         ),
       );
-      expect(find.byType(Padding), findsOneWidget);
-      expect(find.byType(CachedImageFromNetwork), findsOneWidget);
       expect(find.byType(Row), findsOneWidget);
-      expect(find.byType(Text), findsOneWidget);
-      expect(find.byType(Spacer), findsOneWidget);
-      expect(find.byType(SizedBox), findsNWidgets(2));
+      expect(find.byType(Expanded), findsOneWidget);
+      expect(find.byType(MyText), findsOneWidget);
     });
   });
 }
