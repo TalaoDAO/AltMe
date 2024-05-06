06d83f330fabba27849c8e77af733cebe8dfed9f
Bibash Shrestha <bibashshrestha@gmail.com>
2024-05-06 18:19:07 +0545

    Add-update tests basic widgets

--- test/credentials/models/credential_status_field_test.dart
+++ test/credentials/models/credential_status_field_test.dart
@@ -25,31 +25,50 @@ void main() {
     test('fromJson constructor work correctly', () {
       final credentialStatusField = CredentialStatusField.fromJson(
         <String, dynamic>{
-          'id': 'id',
+          'id': '123',
           'type': 'type',
           'revocationListIndex': 'revocationListIndex',
           'revocationListCredential': 'revocationListCredential',
+          'statusListCredential': 'statusListCredential',
+          'statusListIndex': 'statusListIndex',
+          'statusPurpose': 'statusPurpose',
         },
       );
-      expect(credentialStatusField.id, 'id');
+      expect(credentialStatusField.id, '123');
       expect(credentialStatusField.type, 'type');
       expect(credentialStatusField.revocationListIndex, 'revocationListIndex');
       expect(
         credentialStatusField.revocationListCredential,
         'revocationListCredential',
       );
+      expect(
+        credentialStatusField.statusListCredential,
+        'statusListCredential',
+      );
+      expect(credentialStatusField.statusListIndex, 'statusListIndex');
+      expect(credentialStatusField.statusPurpose, 'statusPurpose');
     });
 
     test('toJson constructor work correctly', () {
-      final json = <String, dynamic>{
-        'id': 'id',
-        'type': 'type',
-        'revocationListIndex': 'revocationListIndex',
-        'revocationListCredential': 'revocationListCredential',
-      };
-      final credentialStatusField = CredentialStatusField.fromJson(json);
+      final credentialStatusField = CredentialStatusField(
+        'id',
+        'type',
+        'revocationListIndex',
+        'revocationListCredential',
+        'statusListCredential',
+        'statusListIndex',
+        'statusPurpose',
+      );
 
-      expect(credentialStatusField.toJson(), json);
+      final json = credentialStatusField.toJson();
+
+      expect(json['id'], 'id');
+      expect(json['type'], 'type');
+      expect(json['revocationListIndex'], 'revocationListIndex');
+      expect(json['revocationListCredential'], 'revocationListCredential');
+      expect(json['statusListCredential'], 'statusListCredential');
+      expect(json['statusListIndex'], 'statusListIndex');
+      expect(json['statusPurpose'], 'statusPurpose');
     });
 
     test('empty constructor work properly', () {
