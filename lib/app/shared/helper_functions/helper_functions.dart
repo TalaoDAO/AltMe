d7b7b4e5dd6d6afd333acf75be1b592a70653d33
Bibash Shrestha <bibashshrestha@gmail.com>
2024-07-25 11:40:27 +0545

    feat: Update error responses #2573

--- lib/app/shared/helper_functions/helper_functions.dart
+++ lib/app/shared/helper_functions/helper_functions.dart
@@ -1252,7 +1252,6 @@ MessageHandler getMessageHandler(dynamic e) {
 
 ResponseString getErrorResponseString(String errorString) {
   switch (errorString) {
-    case 'invalid_request':
     case 'invalid_request_uri':
     case 'invalid_request_object':
       return ResponseString.RESPONSE_STRING_invalidRequest;
