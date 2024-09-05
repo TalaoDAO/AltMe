2b3fd52164b370ee67f77c9585bb511155217e0f
Bibash Shrestha <bibashshrestha@gmail.com>
2024-09-05 20:37:11 +0545

    feat: Fix notification issue and make chat only custom

--- lib/enterprise/cubit/enterprise_cubit.dart
+++ lib/enterprise/cubit/enterprise_cubit.dart
@@ -526,6 +526,7 @@ class EnterpriseCubit extends Cubit<EnterpriseState> {
 
         if (roomName != savedRoomName) {
           await matrixNotificationCubit.clearRoomIdFromStorage();
+          await matrixNotificationCubit.dispose();
         }
 
         await matrixNotificationCubit.init();
