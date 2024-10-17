5063b8a175da75e5f1f48f106954a833212d5d21
Bibash Shrestha <bibashshrestha@gmail.com>
2024-10-17 16:19:39 +0545

    refactor: Remove multiple activity log

--- lib/dashboard/drawer/src/view/drawer_page.dart
+++ lib/dashboard/drawer/src/view/drawer_page.dart
@@ -180,15 +180,6 @@ class DrawerView extends StatelessWidget {
                       ),
                       const SizedBox(height: Sizes.spaceSmall),
                       DrawerCategoryItem(
-                        title: l10n.activityLog,
-                        subTitle: l10n.activityLogDescription,
-                        onClick: () {
-                          Navigator.of(context)
-                              .push<void>(ActivityLogPage.route());
-                        },
-                      ),
-                      const SizedBox(height: Sizes.spaceSmall),
-                      DrawerCategoryItem(
                         title: l10n.resetWallet,
                         subTitle: l10n.resetWalletDescription,
                         onClick: () {
