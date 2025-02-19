0a36d7c1e14447a5e169fc17d99f7b0d35015e3d
hawkbee1 <googandads@gmail.com>
2025-02-19 14:38:53 +0000

    Display correctly face attribute #3268

--- lib/ldp_vc/ldp_vc.dart
+++ lib/ldp_vc/ldp_vc.dart
@@ -1,3 +1,4 @@
+import 'package:altme/app/app.dart';
 import 'package:altme/dashboard/home/tab_bar/credentials/models/credential_model/credential_model.dart';
 
 class LdpVc {
@@ -7,19 +8,14 @@ class LdpVc {
   String? get getPicture {
     final credentialSubject = credentialModel
         .credentialSupported?['credentialSubject'] as Map<String, dynamic>?;
-    final keyList = ['picture', 'face', 'portrait'];
-    final valueTypeList = [
-      'image/jpeg',
-      'image/png',
-    ];
 // check if member of keyList is a key in credentialSubject. If that's the case check if value contains value_type with value in valueTypeList
-    for (final key in keyList) {
+    for (final key in Parameters.pictureOnCardKeyList) {
       final keyPresent = credentialSubject?.containsKey(key) ?? false;
       if (keyPresent) {
         final value = credentialSubject?[key];
         if (value is Map<String, dynamic>) {
           final valueType = value['value_type'];
-          if (valueTypeList.contains(valueType)) {
+          if (Parameters.pictureOnCardValueTypeList.contains(valueType)) {
             return credentialModel.data['credentialSubject'][key] as String?;
           }
         }
