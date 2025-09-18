aa23c504ee73232725692c79f2fe1ba8923dc276
hawkbee1 <googandads@gmail.com>
2025-09-18 11:04:31 +0000

    feat: Update dependencies and add no_screenshot plugin
    - Downgraded `build_runner` version in multiple packages to ^2.4.13.
    - Updated `crypto_keys` dependency version to ^0.3.0+1.
    - Downgraded `dart_jsonwebtoken` to ^2.17.0 in `oidc4vc` and `pubspec.yaml`.
    - Downgraded `jose_plus` to ^0.4.6 in `pubspec.yaml`.
    - Updated `json_path` to ^0.7.6 in `pubspec.yaml`.
    - Updated `test` dependency version to ^1.26.2 in `secure_jwt_signer` and `secure_storage`.
    - Updated `flutter_test` and `mockito` versions in `pubspec.yaml`.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import flutter_image_compress_macos
 import flutter_secure_storage_darwin
 import local_auth_darwin
 import mobile_scanner
+import no_screenshot
 import open_file_mac
 import package_info_plus
 import path_provider_foundation
@@ -38,6 +39,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  NoScreenshotPlugin.register(with: registry.registrar(forPlugin: "NoScreenshotPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
