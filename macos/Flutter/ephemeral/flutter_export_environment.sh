bb040ded76a79d7338a9c72e056678dc09d246c0
hawkbee1 <googandads@gmail.com>
2025-09-30 12:07:32 +0200

    no sandboxing for xcode build

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/volume/AltMe"
+export "FLUTTER_ROOT=/Users/romualdbarbe/Documents/install-flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/romualdbarbe/Documents/tmp/spruceid/AltMe"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.3.4"
