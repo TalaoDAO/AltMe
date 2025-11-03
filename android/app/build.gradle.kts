26369485b85b2e88b0e4ab6e264b6989eb551c01
hawkbee1 <googandads@gmail.com>
2025-11-03 18:11:08 +0000

    ABI filter with new gradle notation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -83,9 +83,10 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            ndk {
-                abiFilters "x86_64"
-                }
+            // Clear the automatically set filters.
+            ndk.abiFilters.clear()
+            // Set your custom filters.
+            ndk.abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
         }
         debug {
             signingConfig = signingConfigs.getByName("debug")
