4d9407b0970439519ca58bff025a70e7e6b8351d
hawkbee1 <googandads@gmail.com>
2025-11-06 09:42:07 +0000

    merge futter update

--- lib/dashboard/profile/models/profile.g.dart
+++ lib/dashboard/profile/models/profile.g.dart
@@ -51,6 +51,7 @@ const _$WalletProtectionTypeEnumMap = {
 const _$ProfileTypeEnumMap = {
   ProfileType.defaultOne: 'defaultOne',
   ProfileType.ebsiV3: 'ebsiV3',
+  ProfileType.ebsiV4: 'ebsiV4',
   ProfileType.diipv3: 'diipv3',
   ProfileType.diipv4: 'diipv4',
   ProfileType.europeanWallet: 'europeanWallet',
