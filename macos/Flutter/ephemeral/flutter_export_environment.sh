4ef884474c7dbce00de6329212c2dc8676120dff
hawkbee1 <googandads@gmail.com>
2025-12-26 18:56:22 +0100

    update ios settings for sandboxing

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/volume/AltMe"
+export "FLUTTER_ROOT=/Users/romualdbarbe/Documents/install-flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/romualdbarbe/Documents/tmp/spruceid/AltMe"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=3.4.0"
-export "FLUTTER_BUILD_NUMBER=5672"
-export "FLUTTER_CLI_BUILD_MODE=debug"
+export "FLUTTER_BUILD_NAME=3.4.1"
+export "FLUTTER_BUILD_NUMBER=5673"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
