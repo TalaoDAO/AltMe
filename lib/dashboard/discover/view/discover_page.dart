1935ae1b5b232cb52ea4d747e89349fd84905310
hawkbee1 <googandads@gmail.com>
2026-01-21 11:37:47 +0000

    Discover screen adjustment

--- lib/dashboard/discover/view/discover_page.dart
+++ lib/dashboard/discover/view/discover_page.dart
@@ -1,6 +1,7 @@
 import 'package:altme/app/app.dart';
 import 'package:altme/credentials/credentials.dart';
 import 'package:altme/dashboard/dashboard.dart';
+import 'package:altme/dashboard/home/tab_bar/tab_controller/widgets/compellio_content_tab.dart';
 import 'package:altme/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -44,112 +45,114 @@ class _DiscoverPageState extends State<DiscoverPage> {
             0,
           ),
       backgroundColor: Colors.transparent,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 0),
-            child: Text(
-              'Check your credentials cards',
-              maxLines: 3,
-              style: Theme.of(
-                context,
-              ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w100),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: SizedBox(
-              width: 120,
-              child: MyTab(
-                text: l10n.cards,
-                icon: IconStrings.cards,
-                isSelected: true,
-                onPressed: () {
-                  if (context.read<HomeCubit>().state.homeStatus ==
-                      HomeStatus.hasNoWallet) {
-                    showDialog<void>(
-                      context: context,
-                      builder: (_) => const WalletDialog(),
-                    );
-                    return;
-                  }
-                  context.read<HomeTabbarCubit>().setIndex(0);
-                  context.read<DashboardCubit>().onPageChanged(0);
-                  pageController.jumpToPage(0);
-                },
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 0),
+              child: Text(
+                'Check your credentials cards',
+                maxLines: 3,
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w100),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 0),
-            child: Text(
-              'See your NFTs',
-              maxLines: 3,
-              style: Theme.of(
-                context,
-              ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w100),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: SizedBox(
-              width: 120,
-              child: MyTab(
-                text: l10n.nfts,
-                icon: IconStrings.ghost,
-                isSelected: true,
-                onPressed: () {
-                  if (context.read<HomeCubit>().state.homeStatus ==
-                      HomeStatus.hasNoWallet) {
-                    showDialog<void>(
-                      context: context,
-                      builder: (_) => const WalletDialog(),
-                    );
-                    return;
-                  }
-                  context.read<HomeTabbarCubit>().setIndex(1);
-                  context.read<DashboardCubit>().onPageChanged(0);
-                  pageController.jumpToPage(0);
-                },
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                width: 130,
+                child: CompellioContentTab(
+                  text: l10n.cards,
+                  icon: IconStrings.cards,
+                  isSelected: true,
+                  onPressed: () {
+                    if (context.read<HomeCubit>().state.homeStatus ==
+                        HomeStatus.hasNoWallet) {
+                      showDialog<void>(
+                        context: context,
+                        builder: (_) => const WalletDialog(),
+                      );
+                      return;
+                    }
+                    context.read<HomeTabbarCubit>().setIndex(0);
+                    context.read<DashboardCubit>().onPageChanged(0);
+                    pageController.jumpToPage(0);
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 0),
-            child: Text(
-              'Get started with your crypto coins',
-              maxLines: 3,
-              style: Theme.of(
-                context,
-              ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w100),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: SizedBox(
-              width: 120,
-              child: MyTab(
-                text: l10n.coins,
-                icon: IconStrings.health,
-                isSelected: true,
-                onPressed: () {
-                  if (context.read<HomeCubit>().state.homeStatus ==
-                      HomeStatus.hasNoWallet) {
-                    showDialog<void>(
-                      context: context,
-                      builder: (_) => const WalletDialog(),
-                    );
-                    return;
-                  }
-                  context.read<HomeTabbarCubit>().setIndex(2);
-                  context.read<DashboardCubit>().onPageChanged(0);
-                  pageController.jumpToPage(0);
-                },
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 0),
+              child: Text(
+                'See your NFTs',
+                maxLines: 3,
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w100),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                width: 130,
+                child: CompellioContentTab(
+                  text: l10n.nfts,
+                  icon: IconStrings.ghost,
+                  isSelected: true,
+                  onPressed: () {
+                    if (context.read<HomeCubit>().state.homeStatus ==
+                        HomeStatus.hasNoWallet) {
+                      showDialog<void>(
+                        context: context,
+                        builder: (_) => const WalletDialog(),
+                      );
+                      return;
+                    }
+                    context.read<HomeTabbarCubit>().setIndex(1);
+                    context.read<DashboardCubit>().onPageChanged(0);
+                    pageController.jumpToPage(0);
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 0),
+              child: Text(
+                'Get started with your crypto coins',
+                maxLines: 3,
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w100),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                width: 130,
+                child: CompellioContentTab(
+                  text: l10n.coins,
+                  icon: IconStrings.health,
+                  isSelected: true,
+                  onPressed: () {
+                    if (context.read<HomeCubit>().state.homeStatus ==
+                        HomeStatus.hasNoWallet) {
+                      showDialog<void>(
+                        context: context,
+                        builder: (_) => const WalletDialog(),
+                      );
+                      return;
+                    }
+                    context.read<HomeTabbarCubit>().setIndex(2);
+                    context.read<DashboardCubit>().onPageChanged(0);
+                    pageController.jumpToPage(0);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/dashboard/home/tab_bar/tab_controller/widgets/compellio_content_tab.dart
@@ -0,0 +1,65 @@
+import 'package:altme/app/app.dart';
+
+import 'package:flutter/material.dart';
+
+class CompellioContentTab extends StatelessWidget {
+  const CompellioContentTab({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.isSelected,
+    required this.onPressed,
+  });
+
+  final String text;
+  final String icon;
+  final bool isSelected;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.spaceNormal,
+          vertical: Sizes.spaceNormal,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.normalRadius),
+          color: isSelected
+              ? Theme.of(context).colorScheme.primaryContainer
+              : Theme.of(context).colorScheme.surfaceContainer,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            if (isSelected)
+              Image.asset(
+                icon,
+                height: Sizes.icon,
+                colorBlendMode: BlendMode.color,
+                color: Theme.of(context).colorScheme.primaryContainer,
+              )
+            else
+              Image.asset(icon, height: Sizes.icon),
+            const SizedBox(width: Sizes.spaceXSmall),
+            Text(
+              text,
+              maxLines: 1,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: isSelected
+                    ? Theme.of(context).colorScheme.onPrimaryContainer
+                    : Theme.of(context).colorScheme.inverseSurface,
+              ),
+              overflow: TextOverflow.fade,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
