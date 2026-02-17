bcad5dc12fd4bce715ce8fe41baa22e8ecf74b88
hawkbee1 <googandads@gmail.com>
2026-02-17 14:11:22 +0000

    Android package name is  com.compellio.wallet

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "wallet.compellio.com"
+        applicationId = "com.compellio.wallet"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
